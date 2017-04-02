110cd9c6da403e71202e7de863f17c9cdea897b7
omkar <omkargharat10@gmail.com>
2017-04-02 21:13:33 +0530

    Added device FCM registration

--- app/src/main/java/com/noqapp/client/network/NoQueueFirbaseInstanceServices.java
+++ app/src/main/java/com/noqapp/client/network/NoQueueFirbaseInstanceServices.java
@@ -4,6 +4,9 @@ import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.noqapp.client.model.DeviceModel;
+import com.noqapp.client.presenter.beans.body.DeviceToken;
+import com.noqapp.client.views.activities.LaunchActivity;
 
 /**
  * Created by omkar on 4/2/17.
@@ -20,5 +23,15 @@ public class NoQueueFirbaseInstanceServices extends FirebaseInstanceIdService {
         String refereshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refress Token ::: " + refereshedToken);
 
+        sendRegistrationToServer(refereshedToken);
+
+    }
+
+    private  void sendRegistrationToServer(String refreshToken)
+    {
+        DeviceModel deviceModel = new DeviceModel();
+        DeviceToken deviceToken = new DeviceToken();
+        deviceToken.setFcmToken(refreshToken);
+        deviceModel.register(LaunchActivity.DID, deviceToken);
     }
 }
