254ef24db612bbfc62d38ea8183d0f3aee2255c8
hth <hth@users.noreply.github.com>
2017-04-02 18:47:14 +0530

    Added device registration

---
+++ app/src/main/java/com/noqapp/client/presenter/beans/DeviceRegistered.java
@@ -0,0 +1,34 @@
+package com.noqapp.client.presenter.beans;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+/**
+ * User: hitender
+ * Date: 4/2/17 6:44 PM
+ */
+
+@SuppressWarnings ({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable"
+})
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonInclude (JsonInclude.Include.NON_NULL)
+public class DeviceRegistered {
+
+    @JsonProperty("r")
+    private int registered;
+
+    public int getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(int registered) {
+        this.registered = registered;
+    }
+}
+
