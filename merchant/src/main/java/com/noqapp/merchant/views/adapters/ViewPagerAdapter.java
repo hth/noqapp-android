39eba73da5f1d721b05c08594cdd3c6450c3ee4a
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2017-04-23 10:31:45 +0530

    Title : Client -Join queue & Scan queue screens label change and value swaped ; Merchant- button label condition added

--- merchant/src/main/java/com/noqapp/merchant/views/adapters/ViewPagerAdapter.java
+++ merchant/src/main/java/com/noqapp/merchant/views/adapters/ViewPagerAdapter.java
@@ -15,7 +15,6 @@ import android.widget.TextView;
 
 import com.noqapp.merchant.R;
 import com.noqapp.merchant.model.ManageQueueModel;
-import com.noqapp.merchant.model.types.QueueStatusEnum;
 import com.noqapp.merchant.model.types.QueueUserStateEnum;
 import com.noqapp.merchant.presenter.beans.JsonTopic;
 import com.noqapp.merchant.presenter.beans.body.Served;
@@ -49,11 +48,6 @@ public class ViewPagerAdapter  extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
 
-        // Declare Variables
-        TextView tv_current_value;
-        TextView tv_total_value;
-        TextView tv_title;
-        Button btn_skip,btn_next;
 
 
         inflater = (LayoutInflater) context
@@ -61,16 +55,37 @@ public class ViewPagerAdapter  extends PagerAdapter {
         View itemView = inflater.inflate(R.layout.viewpager_item, container,
                 false);
 
-        // Locate the TextViews in viewpager_item.xml
-        tv_current_value = (TextView) itemView.findViewById(R.id.tv_current_value);
-        tv_total_value = (TextView) itemView.findViewById(R.id.tv_total_value);
-        tv_title= (TextView) itemView.findViewById(R.id.tv_title);
+        TextView tv_current_value = (TextView) itemView.findViewById(R.id.tv_current_value);
+        TextView tv_total_value = (TextView) itemView.findViewById(R.id.tv_total_value);
+        TextView tv_title= (TextView) itemView.findViewById(R.id.tv_title);
+        Button btn_skip = (Button) itemView.findViewById(R.id.btn_skip);
+        Button btn_next = (Button) itemView.findViewById(R.id.btn_next);
         final JsonTopic lq = arrayList.get(position);
         tv_current_value.setText(String.valueOf(lq.getServingNumber()));
         tv_total_value.setText(String.valueOf(lq.getToken()));
         tv_title.setText(lq.getDisplayName());
+        String status = lq.getQueueStatus().getDescription();
+        switch (status){
 
-        btn_skip = (Button) itemView.findViewById(R.id.btn_skip);
+            case  "Start":
+                btn_next.setText("Start");
+                break;
+            case  "Re-Start":
+                btn_next.setText("Continue");
+                break;
+            case  "Next":
+                btn_next.setText("Next");
+                break;
+            case  "Done":
+                btn_next.setText("Done");
+                break;
+            case  "Closed":
+                btn_next.setText("Closed");
+                break;
+        }
+
+
+
         btn_skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,10 +100,11 @@ public class ViewPagerAdapter  extends PagerAdapter {
                         "$2a$15$ed3VSsc5x367CNiwQ3fKsemHSZUr.D3EVjHVjZ2cBTySc/l7gwPua",served);
             }
         });
-        btn_next = (Button) itemView.findViewById(R.id.btn_next);
+
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                LaunchActivity.getLaunchActivity().progressDialog.show();
                 Served served=new Served();
                 served.setCodeQR(lq.getCodeQR());
                 served.setQueueStatus(lq.getQueueStatus());
@@ -99,7 +115,7 @@ public class ViewPagerAdapter  extends PagerAdapter {
             }
         });
 
-        // Add viewpager_item.xml to ViewPager
+
         ((ViewPager) container).addView(itemView);
 
         return itemView;
