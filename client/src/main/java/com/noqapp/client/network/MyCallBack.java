9d841caaa61a8e0d9f92b18123792abf6b837188
hth <hth@users.noreply.github.com>
2017-04-25 15:25:20 +0530

    Added condition check for 404 error

--- client/src/main/java/com/noqapp/client/network/MyCallBack.java
+++ client/src/main/java/com/noqapp/client/network/MyCallBack.java
@@ -2,6 +2,8 @@ package com.noqapp.client.network;
 
 
 
+import android.util.Log;
+
 import com.noqapp.client.views.activities.LaunchActivity;
 
 import retrofit2.Call;
@@ -13,16 +15,18 @@ import retrofit2.Response;
  */
 
 public class MyCallBack<T> implements Callback<T> {
+    private static final String TAG = MyCallBack.class.getSimpleName();
 
     @Override
     public void onResponse(Call<T> call, Response<T> response) {
         LaunchActivity.getLaunchActivity().dismissProgress();
+        if (response.code() == 404) {
+            Log.e(TAG, "404 Not found link=" + response.raw().request().url());
+        }
     }
 
     @Override
     public void onFailure(Call<T> call, Throwable t) {
         LaunchActivity.getLaunchActivity().dismissProgress();
     }
-
-
 }
