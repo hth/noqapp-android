e332e2a954f537739591c82ae60e42b70273d419
hth <hth@users.noreply.github.com>
2017-05-01 12:57:59 +0530

    added phone lib

--- client/src/main/java/com/noqapp/client/helper/ShowAlertInformation.java
+++ client/src/main/java/com/noqapp/client/helper/ShowAlertInformation.java
@@ -10,24 +10,16 @@ import com.noqapp.client.R;
 public class ShowAlertInformation {
 
     public static void showDialog(Context context, String Title, String Message) {
-
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
-                context);
-
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
         alertDialog.setTitle(Title);
-
         alertDialog.setMessage(Message);
-
         alertDialog.setIcon(R.mipmap.ic_launcher);
-
         alertDialog.setNegativeButton("OK",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-
                         dialog.cancel();
                     }
                 });
-
         alertDialog.show();
     }
 
