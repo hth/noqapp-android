b870d87b464f8c6b4aa71e017286ec453a96103c
hth <hth@users.noreply.github.com>
2017-05-11 00:59:12 +0530

    foratting

--- client/src/main/java/com/noqapp/client/views/adapters/ListQueueAdapter.java
+++ client/src/main/java/com/noqapp/client/views/adapters/ListQueueAdapter.java
@@ -18,112 +18,112 @@ import java.util.List;
 
 public class ListQueueAdapter extends BaseExpandableListAdapter {
 
-	private Context context;
-	private List<String> listDataHeader; // header titles
-	// child data in format of header title, child title
-	private HashMap<String, List<JsonTokenAndQueue>> listDataChild;
+    private Context context;
+    private List<String> listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<JsonTokenAndQueue>> listDataChild;
 
-	public ListQueueAdapter(Context context, List<String> listDataHeader,
+    public ListQueueAdapter(Context context, List<String> listDataHeader,
                             HashMap<String, List<JsonTokenAndQueue>> listChildData) {
-		this.context = context;
-		this.listDataHeader = listDataHeader;
-		this.listDataChild = listChildData;
-	}
+        this.context = context;
+        this.listDataHeader = listDataHeader;
+        this.listDataChild = listChildData;
+    }
 
-	@Override
-	public Object getChild(int groupPosition, int childPosititon) {
-		return this.listDataChild.get(this.listDataHeader.get(groupPosition))
-				.get(childPosititon);
-	}
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this.listDataChild.get(this.listDataHeader.get(groupPosition))
+                .get(childPosititon);
+    }
 
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		return childPosition;
-	}
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
 
-	@Override
-	public View getChildView(int groupPosition, final int childPosition,
-			boolean isLastChild, View convertView, ViewGroup parent) {
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
 
 
-		JsonTokenAndQueue queue=(JsonTokenAndQueue)getChild(groupPosition, childPosition);
+        JsonTokenAndQueue queue = (JsonTokenAndQueue) getChild(groupPosition, childPosition);
 
-		if (convertView == null) {
-			LayoutInflater infalInflater = (LayoutInflater) this.context
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			if(groupPosition==0)
-				convertView = infalInflater.inflate(R.layout.listitem_currentqueue, null);
-			else
-				convertView = infalInflater.inflate(R.layout.listitem_historyqueue, null);
-		}
-		TextView txtnumber = (TextView) convertView.findViewById(R.id.txtNumber);
-		TextView txtStoreName = (TextView) convertView.findViewById(R.id.txtStoreName);
-		TextView txtStorePhoneNumber = (TextView) convertView.findViewById(R.id.txtStorePhoneNo);
-		TextView txtToken = (TextView) convertView.findViewById(R.id.txtToken);
-		txtnumber.setText("#"+String.valueOf(childPosition));
-		txtStoreName.setText(queue.getBusinessName());
-		// show only for current queue not for history
-		txtStorePhoneNumber.setText(PhoneFormatterUtil.formatNumber(queue.getCountryShortName(),queue.getStorePhone()));
-		txtToken.setText(String.valueOf(queue.getToken()));
-		return convertView;
-	}
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (groupPosition == 0)
+                convertView = infalInflater.inflate(R.layout.listitem_currentqueue, null);
+            else
+                convertView = infalInflater.inflate(R.layout.listitem_historyqueue, null);
+        }
+        TextView txtnumber = (TextView) convertView.findViewById(R.id.txtNumber);
+        TextView txtStoreName = (TextView) convertView.findViewById(R.id.txtStoreName);
+        TextView txtStorePhoneNumber = (TextView) convertView.findViewById(R.id.txtStorePhoneNo);
+        TextView txtToken = (TextView) convertView.findViewById(R.id.txtToken);
+        txtnumber.setText("#" + String.valueOf(childPosition));
+        txtStoreName.setText(queue.getBusinessName());
+        // show only for current queue not for history
+        txtStorePhoneNumber.setText(PhoneFormatterUtil.formatNumber(queue.getCountryShortName(), queue.getStorePhone()));
+        txtToken.setText(String.valueOf(queue.getToken()));
+        return convertView;
+    }
 
-	@Override
-	public int getChildrenCount(int groupPosition) {
-		return this.listDataChild.get(this.listDataHeader.get(groupPosition))
-				.size();
-	}
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.listDataChild.get(this.listDataHeader.get(groupPosition))
+                .size();
+    }
 
-	@Override
-	public Object getGroup(int groupPosition) {
-		return this.listDataHeader.get(groupPosition);
-	}
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.listDataHeader.get(groupPosition);
+    }
 
-	@Override
-	public int getGroupCount() {
-		return this.listDataHeader.size();
-	}
+    @Override
+    public int getGroupCount() {
+        return this.listDataHeader.size();
+    }
 
-	@Override
-	public long getGroupId(int groupPosition) {
-		return groupPosition;
-	}
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
 
-	@Override
-	public View getGroupView(int groupPosition, boolean isExpanded,
-			View convertView, ViewGroup parent) {
-		String headerTitle = (String) getGroup(groupPosition);
-		if (convertView == null) {
-			LayoutInflater infalInflater = (LayoutInflater) this.context
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-			if (groupPosition == 0) {
-				convertView = infalInflater.inflate(R.layout.list_group_blank, null);
+            if (groupPosition == 0) {
+                convertView = infalInflater.inflate(R.layout.list_group_blank, null);
 
-			} else {
-				convertView = infalInflater.inflate(R.layout.list_group, null);
-			}
-		}
-		if (groupPosition > 0) {
-			TextView lblListHeader = (TextView) convertView
-					.findViewById(R.id.lblListHeader);
-			lblListHeader.setTypeface(null, Typeface.BOLD);
-			lblListHeader.setText(headerTitle);
-			ImageView ivGroupIndicator = (ImageView) convertView.findViewById(R.id.ivGroupIndicator);
-			ivGroupIndicator.setSelected(isExpanded);
-		}
+            } else {
+                convertView = infalInflater.inflate(R.layout.list_group, null);
+            }
+        }
+        if (groupPosition > 0) {
+            TextView lblListHeader = (TextView) convertView
+                    .findViewById(R.id.lblListHeader);
+            lblListHeader.setTypeface(null, Typeface.BOLD);
+            lblListHeader.setText(headerTitle);
+            ImageView ivGroupIndicator = (ImageView) convertView.findViewById(R.id.ivGroupIndicator);
+            ivGroupIndicator.setSelected(isExpanded);
+        }
 
-		return convertView;
-	}
+        return convertView;
+    }
 
-	@Override
-	public boolean hasStableIds() {
-		return false;
-	}
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
 
-	@Override
-	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		return true;
-	}
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
 
 }
