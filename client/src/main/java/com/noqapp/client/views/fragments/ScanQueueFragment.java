e7186890b1676632f40e57ce61b6271cdc7eab2e
hth <hth@users.noreply.github.com>
2017-05-10 21:04:37 +0530

    renamed tabs

--- client/src/main/java/com/noqapp/client/views/fragments/ScanQueueFragment.java
+++ client/src/main/java/com/noqapp/client/views/fragments/ScanQueueFragment.java
@@ -81,7 +81,7 @@ public class ScanQueueFragment extends NoQueueBaseFragment implements CaptureAct
     public void onResume() {
         super.onResume();
         if (!fromList)// to not modify the actionbar if it is coming from list
-            LaunchActivity.getLaunchActivity().setActionBarTitle("Home");
+            LaunchActivity.getLaunchActivity().setActionBarTitle("ScanQ");
         LaunchActivity.getLaunchActivity().enableDisableBack(false);
     }
 
