05dc508c060fd7067dfe640c1d251a6687f53eb2
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2017-05-18 22:51:18 +0530

    Title : Device UDID issue is resolved for merchant

---
+++ merchant/src/main/java/com/noqapp/merchant/presenter/beans/DeviceRegistered.java
@@ -0,0 +1,43 @@
+package com.noqapp.merchant.presenter.beans;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * User: hitender
+ * Date: 4/2/17 6:44 PM
+ */
+
+@SuppressWarnings({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable"
+})
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonInclude (JsonInclude.Include.NON_NULL)
+public class DeviceRegistered {
+
+    @JsonProperty("r")
+    private int registered;
+
+    public int getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(int registered) {
+        this.registered = registered;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("registered", registered)
+                .toString();
+    }
+}
+
