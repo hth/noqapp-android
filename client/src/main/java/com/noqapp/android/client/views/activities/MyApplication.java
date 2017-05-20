957ebe9f94c2ed73fad2f24cd9081729a91ef18a
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2017-05-20 10:23:55 +0530

    Tile : Application class is added for Digits implementation

---
+++ client/src/main/java/com/noqapp/android/client/views/activities/MyApplication.java
@@ -0,0 +1,12 @@
+package com.noqapp.android.client.views.activities;
+
+import android.app.Application;
+
+
+/**
+ * Created by chandra on 5/20/17.
+ */
+
+public class MyApplication extends Application {
+
+}
