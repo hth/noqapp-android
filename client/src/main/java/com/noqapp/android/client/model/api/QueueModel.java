389acd299d2925a8657f1bf4c4a882c6553d93ab
hth <hth@users.noreply.github.com>
2017-05-27 18:23:41 +0530

    Added secure api for queue

---
+++ client/src/main/java/com/noqapp/android/client/model/api/QueueModel.java
@@ -0,0 +1,81 @@
+package com.noqapp.android.client.model.api;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.noqapp.android.client.model.response.api.QueueService;
+import com.noqapp.android.client.network.RetrofitClient;
+import com.noqapp.android.client.presenter.QueuePresenter;
+import com.noqapp.android.client.presenter.ResponsePresenter;
+import com.noqapp.android.client.presenter.TokenAndQueuePresenter;
+import com.noqapp.android.client.presenter.TokenPresenter;
+import com.noqapp.android.client.presenter.beans.JsonQueue;
+import com.noqapp.android.client.presenter.beans.JsonToken;
+import com.noqapp.android.client.utils.Constants;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * User: hitender
+ * Date: 5/27/17 5:38 PM
+ */
+
+public class QueueModel {
+    private static final String TAG = QueueModel.class.getSimpleName();
+
+    private final static QueueService queueService;
+    public static QueuePresenter queuePresenter;
+    public static TokenPresenter tokenPresenter;
+    public static ResponsePresenter responsePresenter;
+    public static TokenAndQueuePresenter tokenAndQueuePresenter;
+
+    static {
+        queueService = RetrofitClient.getClient().create(QueueService.class);
+    }
+
+    public static void remoteScanQueueState(String did, String mail, String auth, String codeQR) {
+        queueService.remoteScanQueueState(did, Constants.DEVICE_TYPE, mail, auth, codeQR).enqueue(new Callback<JsonQueue>() {
+            @Override
+            public void onResponse(@NonNull Call<JsonQueue> call, @NonNull Response<JsonQueue> response) {
+                if (response.body() != null) {
+                    Log.d("Response", String.valueOf(response.body()));
+                    queuePresenter.queueResponse(response.body());
+                } else {
+                    //TODO something logical
+                    Log.e(TAG, "Get state of queue upon scan");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<JsonQueue> call, @NonNull Throwable t) {
+                Log.e("Response", t.getLocalizedMessage(), t);
+
+                queuePresenter.queueError();
+            }
+        });
+    }
+
+    public static void remoteJoinQueue(String did, String mail, String auth, String codeQR) {
+        queueService.remoteJoinQueue(did, Constants.DEVICE_TYPE, mail, auth, codeQR).enqueue(new Callback<JsonToken>() {
+            @Override
+            public void onResponse(@NonNull Call<JsonToken> call, @NonNull Response<JsonToken> response) {
+                if (response.body() != null && response.body().getError() == null) {
+                    Log.d("Response", response.body().toString());
+                    tokenPresenter.tokenPresenterResponse(response.body());
+                } else {
+                    //TODO something logical
+                    Log.e(TAG, "Failed to join queue" + response.body().getError());
+                    tokenPresenter.tokenPresenterError();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<JsonToken> call, @NonNull Throwable t) {
+                Log.e("Response", t.getLocalizedMessage(), t);
+                tokenPresenter.tokenPresenterError();
+            }
+        });
+    }
+}
--- client/src/main/java/com/noqapp/android/client/model/response/api/QueueService.java
+++ client/src/main/java/com/noqapp/android/client/model/response/api/QueueService.java
@@ -1,10 +1,12 @@
 package com.noqapp.android.client.model.response.api;
 
 import com.noqapp.android.client.presenter.beans.JsonQueue;
+import com.noqapp.android.client.presenter.beans.JsonToken;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -17,13 +19,55 @@ import retrofit2.http.Path;
 public interface QueueService {
 
     //TODO need to work on this
-    @GET("api/token/{codeQR}.json")
+    @GET("api/c/token/{codeQR}.json")
     Call<JsonQueue> getQueueState(
             @Header("X-R-DID")
             String did,
 
             @Header("X-R-DT")
             String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth,
+
+            @Path("codeQR")
+            String codeQR
+    );
+
+    @POST("api/c/token/remote/{codeQR}.json")
+    Call<JsonQueue> remoteScanQueueState(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth,
+
+            @Path("codeQR")
+            String codeQR
+    );
+
+    @POST("api/c/token/remote/queue/{codeQR}.json")
+    Call<JsonToken> remoteJoinQueue(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth,
 
             @Path("codeQR")
             String codeQR
