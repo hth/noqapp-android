57267a53ccae5c15bb9f435a44584d9f56152e22
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2017-07-29 12:20:37 +0530

    Title : Client - Theme dialog implementation done

--- client/src/main/java/com/noqapp/android/client/utils/ShowAlertInformation.java
+++ client/src/main/java/com/noqapp/android/client/utils/ShowAlertInformation.java
@@ -3,27 +3,48 @@ package com.noqapp.android.client.utils;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.noqapp.android.client.R;
 
 
 public class ShowAlertInformation {
 
-    public static void showDialog(Context context, String Title, String Message) {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
-        alertDialog.setTitle(Title);
-        alertDialog.setMessage(Message);
-        alertDialog.setIcon(R.mipmap.launcher);
-        alertDialog.setNegativeButton(context.getString(R.string.ok),
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-        alertDialog.show();
+    public static void showNetworkDialog(Context context) {
+        showThemeDialog(context, context.getString(R.string.networkerror), context.getString(R.string.offline));
     }
 
-    public static void showNetworkDialog(Context context) {
-        showDialog(context, context.getString(R.string.networkerror), context.getString(R.string.offline));
+    public static void showThemeDialog(Context context, String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        builder.setTitle(null);
+        View customDialogView = inflater.inflate(R.layout.dialog_general, null, false);
+        TextView tvtitle = (TextView) customDialogView.findViewById(R.id.tvtitle);
+        TextView tv_msg = (TextView) customDialogView.findViewById(R.id.tv_msg);
+        tvtitle.setText(title);
+        tv_msg.setText(message);
+        builder.setView(customDialogView);
+        final AlertDialog mAlertDialog = builder.create();
+        mAlertDialog.setCanceledOnTouchOutside(false);
+        Button btn_yes = (Button) customDialogView.findViewById(R.id.btn_yes);
+        Button btn_no = (Button) customDialogView.findViewById(R.id.btn_no);
+        btn_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAlertDialog.dismiss();
+            }
+        });
+        btn_yes.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                mAlertDialog.dismiss();
+            }
+        });
+        mAlertDialog.show();
     }
 }
