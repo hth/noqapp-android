85d2e7cc2e61b4daf1cc9308bc737e6676f439ae
hth <hth@users.noreply.github.com>
2017-09-08 13:20:15 -0700

    Added text

--- client/src/main/java/com/noqapp/android/client/views/fragments/InviteFragment.java
+++ client/src/main/java/com/noqapp/android/client/views/fragments/InviteFragment.java
@@ -62,9 +62,10 @@ public class InviteFragment extends NoQueueBaseFragment {
         Bundle bundle = getArguments();
         if (null != bundle) {
             String title = bundle.getString("title", "Invite Friends");
-            String details = bundle.getString("details", "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
-                    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer" +
-                    " took a galley of type and scrambled it to make a type specimen book.");
+            String details = bundle.getString("details", "As a user of NoQApp, you can invite your friends. " +
+                    "On successful registration of your friend, you and your friend both receive 2 free remote joins. " +
+                    "A remote join can be used to join a queue remotely, if a business has opened this feature for " +
+                    "their queue. To keep getting free remote joins, keep sending invites to your friends.");
             String invite_code = bundle.getString("invite_code", "");
 
             tv_title.setText(title);
