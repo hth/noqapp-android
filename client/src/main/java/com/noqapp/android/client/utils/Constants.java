26914d1882c421a555ef6a48aec4584e479629b6
hth <hth@users.noreply.github.com>
2017-10-17 22:21:38 +0530

    updated link

--- client/src/main/java/com/noqapp/android/client/utils/Constants.java
+++ client/src/main/java/com/noqapp/android/client/utils/Constants.java
@@ -39,8 +39,8 @@ public class Constants {
     private static String VERSION_RELEASE;
 
     //Urls
-    public static final String URL_TERM_CONDITION = "https://noqapp.com/beta/terms.html";
-    public static final String URL_PRIVACY_POLICY = "https://noqapp.com/beta/privacy.html";
+    public static final String URL_TERM_CONDITION = "https://noqapp.com/beta/m.terms.html";
+    public static final String URL_PRIVACY_POLICY = "https://noqapp.com/beta/m.privacy.html";
     public static final String URL_ABOUT_US = "https://noqapp.com/beta/our-story.html";
     public static final String URL_HOW_IT_WORKS = "https://noqapp.com/beta/how-it-works.html";
 
