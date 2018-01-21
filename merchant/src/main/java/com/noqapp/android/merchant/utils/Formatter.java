a1de8ae910b05ace84abe1957a14782176071f20
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2018-01-21 19:20:26 +0530

    Title : Merchant- Unlimited token & update time feature added in settings screen.

---
+++ merchant/src/main/java/com/noqapp/android/merchant/utils/Formatter.java
@@ -0,0 +1,80 @@
+package com.noqapp.android.merchant.utils;
+
+import android.util.Log;
+
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * User: chandra
+ * Date: 5/1/17 7:11 PM
+ */
+public class Formatter {
+    private static final String TAG = Formatter.class.getSimpleName();
+    private static final DateFormat formatRFC822 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
+
+    private static final DateFormat df = DateFormat.getDateInstance();
+    private static final TimeZone tz = TimeZone.getTimeZone("UTC");
+
+    private static DateTimeFormatter inputFormatter = DateTimeFormat.forPattern("HHmm");
+    private static DateTimeFormatter outputFormatter = DateTimeFormat.forPattern("HH:mm");
+
+    private Formatter() {
+        formatRFC822.setTimeZone(tz);
+    }
+
+    public static String getFormattedAddress(String address) {
+        if (address.contains(",")) {
+            String[] arr = address.split(",");
+            if (arr.length > 2) {
+                int secondIndex = address.indexOf(',', address.indexOf(',') + 1);
+                return address.substring(0, secondIndex + 1) + "\n" + address.substring(secondIndex + 1, address.length());
+            } else
+                return address;
+        } else {
+            return address;
+        }
+    }
+
+    public static Date getDateFromString(String dateTimeString) {
+        try {
+            return formatRFC822.parse(dateTimeString);
+        } catch (ParseException e) {
+            Log.e(TAG, "Date parsing error reason" + e.getLocalizedMessage());
+        }
+
+        return null;
+    }
+
+    public static String getDateTimeAsString(Date date) {
+        if (null != date) {
+            return DateFormat.getDateTimeInstance().format(date);
+        }
+
+        return "";
+    }
+
+    static LocalTime parseLocalTime(String rawTimestamp) {
+        return inputFormatter.parseLocalTime(rawTimestamp);
+    }
+
+    private static String convertMilitaryTo12HourFormat(String rawTimestamp) {
+        return outputFormatter.print(parseLocalTime(rawTimestamp));
+    }
+
+    static String formatMilitaryTime(int rawTimestamp) {
+        return String.format(Locale.US, "%04d", rawTimestamp);
+    }
+
+    public static String convertMilitaryTo12HourFormat(int rawTimestamp) {
+        return convertMilitaryTo12HourFormat(formatMilitaryTime(rawTimestamp));
+    }
+}
