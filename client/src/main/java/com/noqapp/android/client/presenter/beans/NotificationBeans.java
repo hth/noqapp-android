2c6648f3949b6504db6584319f168ffba995abad
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2018-03-19 14:28:56 +0530

    Title: Notification arrival timing column added in notification table for both merchant & client.

--- client/src/main/java/com/noqapp/android/client/presenter/beans/NotificationBeans.java
+++ client/src/main/java/com/noqapp/android/client/presenter/beans/NotificationBeans.java
@@ -9,16 +9,18 @@ public class NotificationBeans {
     private String title;
     private String msg;
     private String status;
+    private String notificationTime;
 
 
     public NotificationBeans() {
 
     }
 
-    public NotificationBeans(String title, String msg, String status) {
+    public NotificationBeans(String title, String msg, String status, String notificationTime) {
         this.title = title;
         this.msg = msg;
         this.status = status;
+        this.notificationTime =notificationTime;
     }
 
     public String getTitle() {
@@ -48,12 +50,22 @@ public class NotificationBeans {
         return this;
     }
 
+    public String getNotificationTime() {
+        return notificationTime;
+    }
+
+    public NotificationBeans setNotificationTime(String notificationTime) {
+        this.notificationTime = notificationTime;
+        return this;
+    }
+
     @Override
     public String toString() {
         return "NotificationBeans{" +
                 "title='" + title + '\'' +
                 ", msg='" + msg + '\'' +
                 ", status='" + status + '\'' +
+                ", notificationTime='" + notificationTime + '\'' +
                 '}';
     }
 }
