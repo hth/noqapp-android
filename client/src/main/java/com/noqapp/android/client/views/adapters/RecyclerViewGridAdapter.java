3d25e3d0f70dbd3ea8b25f925cffc1c95e099ae9
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2018-04-25 13:44:23 +0530

    Title: Client- Home screen UI refine; flow implemented for banks; Category screen updated on basis of business type.

--- client/src/main/java/com/noqapp/android/client/views/adapters/RecyclerViewGridAdapter.java
+++ client/src/main/java/com/noqapp/android/client/views/adapters/RecyclerViewGridAdapter.java
@@ -1,6 +1,7 @@
 package com.noqapp.android.client.views.adapters;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -62,14 +63,24 @@ public class RecyclerViewGridAdapter extends RecyclerView.Adapter<RecyclerViewGr
     public void onBindViewHolder(ViewHolder Vholder, final int position) {
         final JsonCategory jsonCategory = categories.get(position);
         List<BizStoreElastic> jsonQueues = queueMap.get(jsonCategory.getBizCategoryId());
-//        BizStoreElastic jsonQueue = null;
-//        if (!jsonQueues.isEmpty()) {
-//            jsonQueue = jsonQueues.get(0);
-//        }
+        BizStoreElastic jsonQueue = null;
+        if (!jsonQueues.isEmpty()) {
+            jsonQueue = jsonQueues.get(0);
+            switch (jsonQueue.getBusinessType()) {
+                case BK:
+                    Vholder.iv_main.setBackground(ContextCompat.getDrawable(context,R.drawable.bank_bg));
+                    break;
+                default:
+                    Vholder.iv_main.setBackground(ContextCompat.getDrawable(context,R.drawable.dep_bg));
+            }
+
+        }
         Vholder.tv_title.setText(jsonCategory.getCategoryName());
 //        Picasso.with(context)
 //                .load("https://noqapp.com/imgs/240x120/a.jpeg")
 //                .into(Vholder.iv_main);
+
+
         Vholder.card_view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
