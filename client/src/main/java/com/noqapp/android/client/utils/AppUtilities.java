7a63d7ad7e96135e54150bdb70270e1d515a8853
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2018-05-17 16:31:41 +0530

    Title: Client- delivery address & store address distance validation added, menu added for transaction history

--- client/src/main/java/com/noqapp/android/client/utils/AppUtilities.java
+++ client/src/main/java/com/noqapp/android/client/utils/AppUtilities.java
@@ -256,8 +256,22 @@ public class AppUtilities {
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         float dist = (float) (earthRadius * c);
 
-        return String.valueOf(round(dist / 1000)) + " km";// distance in km
+        return String.valueOf(calculateDistance(lat1,lng1,lat2,lng2)) + " km";// distance in km
     }
+    public static double calculateDistance(float lat1, float lng1, float lat2, float lng2) {
+        double earthRadius = 6371000; //meters
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        float dist = (float) (earthRadius * c);
+
+        return round(dist / 1000);// distance in km
+    }
+
+
 
 
     public static void changeLanguage(String language) {
