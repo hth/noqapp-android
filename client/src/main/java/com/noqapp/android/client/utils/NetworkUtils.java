c45d2c9224af4553d3bf05420e849695a5880b83
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2018-07-25 18:45:44 +0530

    Title: Client- network event bus implementation done

---
+++ client/src/main/java/com/noqapp/android/client/utils/NetworkUtils.java
@@ -0,0 +1,57 @@
+package com.noqapp.android.client.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtils {
+
+    public static int TYPE_WIFI = 1;
+    public static int TYPE_MOBILE = 2;
+    public static int TYPE_NOT_CONNECTED = 0;
+
+    public static int getConnectivityStatus(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return TYPE_WIFI;
+
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return TYPE_MOBILE;
+        }
+        return TYPE_NOT_CONNECTED;
+    }
+
+    public static boolean getConnectivityStatusString(Context context) {
+        boolean isNetwork = false;
+        int conn = getConnectivityStatus(context);
+        String status = null;
+        if (conn == TYPE_WIFI) {
+            status = "Wifi enabled";
+            return true;
+        } else if (conn == TYPE_MOBILE) {
+            status = "Mobile data enabled";
+            return true;
+        } else if (conn == TYPE_NOT_CONNECTED) {
+            status = "Not connected to Internet";
+            return false;
+        }
+        return isNetwork;
+    }
+
+    public static boolean isConnectingToInternet(Context _context) {
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+        }
+        return false;
+    }
+}
