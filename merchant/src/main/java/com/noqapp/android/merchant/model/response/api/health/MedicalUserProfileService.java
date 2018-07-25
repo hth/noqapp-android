51aa1d29ce0145f83988b7d8f0082ae4a3e4b667
hth <hth@users.noreply.github.com>
2018-07-25 17:59:49 +0530

    Fetch client profile

---
+++ merchant/src/main/java/com/noqapp/android/merchant/model/response/api/health/MedicalUserProfileService.java
@@ -0,0 +1,34 @@
+package com.noqapp.android.merchant.model.response.api.health;
+
+import com.noqapp.android.common.beans.JsonResponse;
+import com.noqapp.android.merchant.presenter.beans.body.FindMedicalProfile;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+/**
+ * User: hitender
+ * Date: 7/25/18 5:55 PM
+ */
+public interface MedicalUserProfileService {
+
+    @POST("api/m/h/medicalUserProfile/fetch.json")
+    Call<JsonResponse> fetch(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth,
+
+            @Body
+            FindMedicalProfile findMedicalProfile
+    );
+}
