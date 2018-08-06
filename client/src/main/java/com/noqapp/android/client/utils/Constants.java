077eef79793ccbaae3cf6c7fdc537613bfce56c0
hth <hth@users.noreply.github.com>
2018-08-06 21:17:25 +0530

    increased build number

--- client/src/main/java/com/noqapp/android/client/utils/Constants.java
+++ client/src/main/java/com/noqapp/android/client/utils/Constants.java
@@ -63,7 +63,7 @@ public class Constants {
         if (StringUtils.isBlank(VERSION_RELEASE)) {
             switch (BuildConfig.BUILD_TYPE) {
                 case "debug":
-                    VERSION_RELEASE = "1.2.0";
+                    VERSION_RELEASE = "1.2.100";
                     break;
                 default:
                     VERSION_RELEASE = BuildConfig.VERSION_NAME;
