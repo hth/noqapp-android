b4541a24ba93e2de5c49a140c9f976c4e98d2384
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2018-10-10 15:07:09 +0530

    Title: Client - UI changes

--- client/src/main/java/com/noqapp/android/client/views/activities/UserProfileEditActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/UserProfileEditActivity.java
@@ -175,7 +175,7 @@ public class UserProfileEditActivity extends ProfileActivity implements View.OnC
             case R.id.tv_male:
                 gender = "M";
                 tv_female.setBackgroundResource(R.drawable.square_white_bg_drawable);
-                tv_male.setBackgroundResource(R.drawable.gender_redbg);
+                tv_male.setBackgroundResource(R.drawable.blue_gradient);
                 SpannableString ss = new SpannableString("Male  ");
                 Drawable d = getResources().getDrawable(R.drawable.check_white);
                 d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
@@ -188,7 +188,7 @@ public class UserProfileEditActivity extends ProfileActivity implements View.OnC
                 break;
             case R.id.tv_female:
                 gender = "F";
-                tv_female.setBackgroundResource(R.drawable.gender_redbg);
+                tv_female.setBackgroundResource(R.drawable.blue_gradient);
                 tv_male.setBackgroundResource(R.drawable.square_white_bg_drawable);
                 tv_female.setCompoundDrawablePadding(0);
                 tv_male.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
