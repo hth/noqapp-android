6ec76f89728c586e7ac358f2be48cde879dd896c
hth <hth@users.noreply.github.com>
2018-10-22 10:53:23 +0530

    added api to list store products

--- merchant/src/main/java/com/noqapp/android/merchant/model/response/api/store/StoreProductService.java
+++ merchant/src/main/java/com/noqapp/android/merchant/model/response/api/store/StoreProductService.java
@@ -1,4 +1,26 @@
 package com.noqapp.android.merchant.model.response.api.store;
 
-public class StoreProductService {
+import com.noqapp.android.merchant.presenter.beans.JsonTopicList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+public interface StoreProductService {
+
+
+    @GET("api/m/s/product/store/{codeQR}.json")
+    Call<JsonStore> storeProduct(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth
+    );
 }
