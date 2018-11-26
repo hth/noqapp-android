00639c88aea8c306d0b81cd7bce883d98e0765c4
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2018-11-26 13:02:33 +0530

    Title:Merchant HealthCare-  Preference test screen converted to tab based screen; Swipable test screen feature added, data filter on basis of screen; data submission on basis of screen

--- merchant/src/healthcare/java/com/noqapp/android/merchant/views/activities/PreferenceActivity.java
+++ merchant/src/healthcare/java/com/noqapp/android/merchant/views/activities/PreferenceActivity.java
@@ -1,42 +1,35 @@
 package com.noqapp.android.merchant.views.activities;
 
-import com.noqapp.android.common.beans.ErrorEncounteredJson;
-import com.noqapp.android.common.beans.JsonResponse;
-import com.noqapp.android.common.model.types.category.HealthCareServiceEnum;
-import com.noqapp.android.merchant.R;
-import com.noqapp.android.merchant.interfaces.FilePresenter;
-import com.noqapp.android.merchant.interfaces.MasterLabPresenter;
-import com.noqapp.android.merchant.model.MasterLabModel;
-import com.noqapp.android.merchant.presenter.beans.JsonMasterLab;
-import com.noqapp.android.merchant.utils.AppUtils;
-import com.noqapp.android.merchant.utils.Constants;
-import com.noqapp.android.merchant.utils.ErrorResponseHandler;
-import com.noqapp.android.merchant.utils.UserUtils;
-import com.noqapp.android.merchant.views.adapters.MultiSelectListAdapter;
-import com.noqapp.android.merchant.views.pojos.DataObj;
-
-import org.rauschig.jarchivelib.Archiver;
-import org.rauschig.jarchivelib.ArchiverFactory;
-
 import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
+
+import com.noqapp.android.common.beans.ErrorEncounteredJson;
+import com.noqapp.android.common.model.types.category.HealthCareServiceEnum;
+import com.noqapp.android.merchant.R;
+import com.noqapp.android.merchant.interfaces.FilePresenter;
+import com.noqapp.android.merchant.model.MasterLabModel;
+import com.noqapp.android.merchant.presenter.beans.JsonMasterLab;
+import com.noqapp.android.merchant.utils.AppUtils;
+import com.noqapp.android.merchant.utils.ErrorResponseHandler;
+import com.noqapp.android.merchant.utils.UserUtils;
+import com.noqapp.android.merchant.views.adapters.MenuHeaderAdapter;
+import com.noqapp.android.merchant.views.adapters.TabViewPagerAdapter;
+import com.noqapp.android.merchant.views.fragments.PreferenceHCServiceFragment;
+
+import org.rauschig.jarchivelib.Archiver;
+import org.rauschig.jarchivelib.ArchiverFactory;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -44,100 +37,94 @@ import java.io.FileReader;
 import java.util.ArrayList;
 
 
-public class PreferenceActivity extends AppCompatActivity implements MasterLabPresenter, FilePresenter {
+public class PreferenceActivity extends AppCompatActivity implements FilePresenter, MenuHeaderAdapter.OnItemClickListener {
     private final int STORAGE_PERMISSION_CODE = 102;
     private final String[] STORAGE_PERMISSION_PERMS = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
-    private ListView lv_tests, lv_all_tests;
-    private AutoCompleteTextView actv_search;
-    private EditText edt_add;
-    private ArrayAdapter<String> listAdapter;
+    private RecyclerView rcv_header;
+    private MenuHeaderAdapter menuAdapter;
     private ProgressDialog progressDialog;
+    private ViewPager viewPager;
+    private long lastPress;
+    private Toast backPressToast;
+    private ArrayList<String> data = new ArrayList<>();
     private ArrayList<JsonMasterLab> masterData = new ArrayList<>();
     private ArrayList<String> masterDataString = new ArrayList<>();
-    private ArrayList<DataObj> checkList = new ArrayList<>();
-    private MultiSelectListAdapter multiSelectListAdapter;
-    private ArrayAdapter<String> actvAdapter;
+    private ArrayList<String> masterDataSono = new ArrayList<>();
+    private ArrayList<String> masterDataScan = new ArrayList<>();
+    private ArrayList<String> masterDataMri = new ArrayList<>();
+    private ArrayList<String> masterDataXray = new ArrayList<>();
+    private ArrayList<String> masterDataPath = new ArrayList<>();
+    private PreferenceHCServiceFragment preferenceSonoFragment, preferencePathFragment, preferenceMriFragment, preferenceScanFragment, preferenceXrayFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (new AppUtils().isTablet(getApplicationContext())) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        } else {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        }
+//        if (new AppUtils().isTablet(getApplicationContext())) {
+//            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//        } else {
+//            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
         initProgress();
-        edt_add = findViewById(R.id.edt_add);
-        lv_tests = findViewById(R.id.lv_tests);
-        lv_all_tests = findViewById(R.id.lv_all_tests);
-        actv_search = findViewById(R.id.actv_search);
-        checkList = LaunchActivity.getSelectedTest();
-        if(null == checkList)
-            checkList = new ArrayList<>();
-        actv_search.setThreshold(1);
-        actv_search.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String value = (String) parent.getItemAtPosition(position);
-                DataObj dataObj = new DataObj();
-                dataObj.setName(value);
-                dataObj.setSelect(false);
-                if (!checkList.contains(dataObj)) {
-                    checkList.add(dataObj);
-                    multiSelectListAdapter.notifyDataSetChanged();
-                    actv_search.setText("");
-                } else {
-                    Toast.makeText(PreferenceActivity.this, "Already selected", Toast.LENGTH_LONG).show();
-                }
-                new AppUtils().hideKeyBoard(PreferenceActivity.this);
-            }
-        });
-        actv_search.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int DRAWABLE_RIGHT = 2;
-                final int DRAWABLE_LEFT = 0;
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    if (event.getRawX() >= (actv_search.getRight() - actv_search.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-                        actv_search.setText("");
-                        return true;
-                    }
-                    if (event.getRawX() <= (20 + actv_search.getLeft() + actv_search.getCompoundDrawables()[DRAWABLE_LEFT].getBounds().width())) {
-                        //performSearch();
-                        return true;
-                    }
-                }
-                return false;
-            }
-        });
+        viewPager = findViewById(R.id.pager);
+        viewPager.setOffscreenPageLimit(5);
+        rcv_header = findViewById(R.id.rcv_header);
+        data.add("MRI");
+        data.add("SCAN");
+        data.add("SONOGRAPHY");
+        data.add("X-RAY");
+        data.add("Pathology");
 
-        multiSelectListAdapter = new MultiSelectListAdapter(this, checkList);
-        lv_tests.setAdapter(multiSelectListAdapter);
-        lv_all_tests.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                DataObj dataObj = new DataObj();
-                dataObj.setName(masterDataString.get(position));
-                dataObj.setSelect(false);
-                if (!checkList.contains(dataObj)) {
-                    checkList.add(dataObj);
-                    multiSelectListAdapter.notifyDataSetChanged();
-                }
-            }
-        });
+        preferencePathFragment = new PreferenceHCServiceFragment();
+        preferencePathFragment.setArguments(getBundle(4));
+        preferenceMriFragment = new PreferenceHCServiceFragment();
+        preferenceMriFragment.setArguments(getBundle(0));
+        preferenceScanFragment = new PreferenceHCServiceFragment();
+        preferenceScanFragment.setArguments(getBundle(1));
+        preferenceSonoFragment = new PreferenceHCServiceFragment();
+        preferenceSonoFragment.setArguments(getBundle(2));
+        preferenceXrayFragment = new PreferenceHCServiceFragment();
+        preferenceXrayFragment.setArguments(getBundle(3));
+        TabViewPagerAdapter adapter = new TabViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(preferenceMriFragment, "FRAG" + 0);
+        adapter.addFragment(preferenceScanFragment, "FRAG" + 1);
+        adapter.addFragment(preferenceSonoFragment, "FRAG" + 2);
+        adapter.addFragment(preferenceXrayFragment, "FRAG" + 3);
+        adapter.addFragment(preferencePathFragment, "FRAG" + 4);
 
-        Button btn_add = findViewById(R.id.btn_add);
-        btn_add.setOnClickListener(new View.OnClickListener() {
+        rcv_header.setHasFixedSize(true);
+        LinearLayoutManager horizontalLayoutManagaer
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rcv_header.setLayoutManager(horizontalLayoutManagaer);
+        rcv_header.setItemAnimator(new DefaultItemAnimator());
+
+
+        menuAdapter = new MenuHeaderAdapter(data, this, this);
+        rcv_header.setAdapter(menuAdapter);
+        menuAdapter.notifyDataSetChanged();
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(0);
+        adapter.notifyDataSetChanged();
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View v) {
-                progressDialog.show();
-                MasterLabModel masterLabModel = new MasterLabModel();
-                masterLabModel.setMasterLabPresenter(PreferenceActivity.this);
-                masterLabModel.add(UserUtils.getDeviceId(), UserUtils.getEmail(), UserUtils.getAuth(), new JsonMasterLab().
-                        setProductName(edt_add.getText().toString()).setProductShortName(edt_add.getText().toString()).setHealthCareService(HealthCareServiceEnum.SONO));
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //saveAllData();
+                rcv_header.smoothScrollToPosition(position);
+                menuAdapter.setSelected_pos(position);
+                menuAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
         if (isStoragePermissionAllowed()) {
@@ -151,42 +138,47 @@ public class PreferenceActivity extends AppCompatActivity implements MasterLabPr
 
     }
 
-    @Override
-    public void masterLabUploadResponse(JsonResponse jsonResponse) {
-        dismissProgress();
-        if (Constants.SUCCESS == jsonResponse.getResponse()) {
-            masterDataString.add(edt_add.getText().toString());
 
-            listAdapter.notifyDataSetChanged();
-            actvAdapter.notifyDataSetChanged();
-            DataObj dataObj = new DataObj();
-            dataObj.setName(edt_add.getText().toString());
-            dataObj.setSelect(false);
-            checkList.add(dataObj);
-            multiSelectListAdapter.notifyDataSetChanged();
-            edt_add.setText("");
-            Toast.makeText(this, "Test updated Successfully", Toast.LENGTH_LONG).show();
+    @Override
+    public void onBackPressed() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastPress > 3000) {
+            backPressToast = Toast.makeText(this, getString(R.string.exit_medical_screen), Toast.LENGTH_LONG);
+            backPressToast.show();
+            lastPress = currentTime;
         } else {
-            Toast.makeText(this, "Failed to update test", Toast.LENGTH_LONG).show();
+            if (backPressToast != null) {
+                backPressToast.cancel();
+            }
+            //super.onBackPressed();
+            finish();
         }
+        // LaunchActivity.setSelectedTest(multiSelectListAdapter.getAllSelectedList());
+    }
+
+
+    private boolean isStoragePermissionAllowed() {
+        //Getting the permission status
+        int result_read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int result_write = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        //If permission is granted returning true
+        if (result_read == PackageManager.PERMISSION_GRANTED && result_write == PackageManager.PERMISSION_GRANTED)
+            return true;
+        //If permission is not granted returning false
+        return false;
     }
 
     @Override
-    public void responseErrorPresenter(ErrorEncounteredJson eej) {
-        dismissProgress();
-        new ErrorResponseHandler().processError(this, eej);
+    public void menuHeaderClick(int pos) {
+        viewPager.setCurrentItem(pos);
+        // saveAllData();
     }
 
-    @Override
-    public void responseErrorPresenter(int errorCode) {
-        dismissProgress();
-        new ErrorResponseHandler().processFailureResponseCode(this, errorCode);
-    }
-
-    @Override
-    public void authenticationFailure() {
-        dismissProgress();
-        AppUtils.authenticationProcessing();
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                STORAGE_PERMISSION_PERMS,
+                STORAGE_PERMISSION_CODE);
     }
 
     private void initProgress() {
@@ -198,6 +190,11 @@ public class PreferenceActivity extends AppCompatActivity implements MasterLabPr
     protected void dismissProgress() {
         if (null != progressDialog && progressDialog.isShowing())
             progressDialog.dismiss();
+    }
+
+    @Override
+    public void fileError() {
+        dismissProgress();
     }
 
     @Override
@@ -233,6 +230,22 @@ public class PreferenceActivity extends AppCompatActivity implements MasterLabPr
                                     masterData.add(new JsonMasterLab().
                                             setProductName(strArray[0]).setProductShortName(strArray[1]).setHealthCareService(HealthCareServiceEnum.valueOf(strArray[2])));
                                     masterDataString.add(strArray[0]);
+                                    if (strArray[2].equals(HealthCareServiceEnum.SCAN.getName())) {
+                                        masterDataScan.add(strArray[0]);
+                                    }
+                                    if (strArray[2].equals(HealthCareServiceEnum.SONO.getName())) {
+                                        masterDataSono.add(strArray[0]);
+                                    }
+                                    if (strArray[2].equals(HealthCareServiceEnum.MRI.getName())) {
+                                        masterDataMri.add(strArray[0]);
+                                    }
+                                    if (strArray[2].equals(HealthCareServiceEnum.XRAY.getName())) {
+                                        masterDataXray.add(strArray[0]);
+                                    }
+                                    if (strArray[2].equals(HealthCareServiceEnum.PATH.getName())) {
+                                        masterDataPath.add(strArray[0]);
+                                    }
+                                    Log.e("data is :", line);
 
                                 }
                             } catch (Exception e) {
@@ -241,11 +254,11 @@ public class PreferenceActivity extends AppCompatActivity implements MasterLabPr
                             }
                         }
                     }
-                    listAdapter = new ArrayAdapter<String>(this,
-                            android.R.layout.simple_list_item_1, masterDataString);
-                    lv_all_tests.setAdapter(listAdapter);
-                    actvAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, masterDataString);
-                    actv_search.setAdapter(actvAdapter);
+                    preferenceXrayFragment.setData(masterDataXray);
+                    preferenceSonoFragment.setData(masterDataSono);
+                    preferenceScanFragment.setData(masterDataScan);
+                    preferenceMriFragment.setData(masterDataMri);
+                    preferencePathFragment.setData(masterDataPath);
                     for (File file : files) {
                         new File(path, file.getName()).delete();
                     }
@@ -259,33 +272,26 @@ public class PreferenceActivity extends AppCompatActivity implements MasterLabPr
     }
 
     @Override
-    public void fileError() {
+    public void responseErrorPresenter(ErrorEncounteredJson eej) {
         dismissProgress();
+        new ErrorResponseHandler().processError(this, eej);
     }
-
-    private boolean isStoragePermissionAllowed() {
-        //Getting the permission status
-        int result_read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        int result_write = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        //If permission is granted returning true
-        if (result_read == PackageManager.PERMISSION_GRANTED && result_write == PackageManager.PERMISSION_GRANTED)
-            return true;
-        //If permission is not granted returning false
-        return false;
-    }
-
-    private void requestStoragePermission() {
-        ActivityCompat.requestPermissions(
-                this,
-                STORAGE_PERMISSION_PERMS,
-                STORAGE_PERMISSION_CODE);
-    }
-
-
 
     @Override
-    public void onBackPressed() {
-        LaunchActivity.setSelectedTest(multiSelectListAdapter.getAllSelectedList());
-        super.onBackPressed();
+    public void responseErrorPresenter(int errorCode) {
+        dismissProgress();
+        new ErrorResponseHandler().processFailureResponseCode(this, errorCode);
+    }
+
+    @Override
+    public void authenticationFailure() {
+        dismissProgress();
+        AppUtils.authenticationProcessing();
+    }
+
+    private Bundle getBundle(int pos) {
+        Bundle b = new Bundle();
+        b.putInt("type", pos);
+        return b;
     }
 }
---
+++ merchant/src/healthcare/java/com/noqapp/android/merchant/views/fragments/PreferenceHCServiceFragment.java
@@ -0,0 +1,199 @@
+package com.noqapp.android.merchant.views.fragments;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.noqapp.android.common.beans.ErrorEncounteredJson;
+import com.noqapp.android.common.beans.JsonResponse;
+import com.noqapp.android.common.model.types.category.HealthCareServiceEnum;
+import com.noqapp.android.merchant.R;
+import com.noqapp.android.merchant.interfaces.MasterLabPresenter;
+import com.noqapp.android.merchant.model.MasterLabModel;
+import com.noqapp.android.merchant.presenter.beans.JsonMasterLab;
+import com.noqapp.android.merchant.utils.AppUtils;
+import com.noqapp.android.merchant.utils.Constants;
+import com.noqapp.android.merchant.utils.ErrorResponseHandler;
+import com.noqapp.android.merchant.utils.UserUtils;
+import com.noqapp.android.merchant.views.activities.LaunchActivity;
+import com.noqapp.android.merchant.views.adapters.MultiSelectListAdapter;
+import com.noqapp.android.merchant.views.pojos.DataObj;
+
+import java.util.ArrayList;
+
+public class PreferenceHCServiceFragment extends Fragment implements MasterLabPresenter {
+
+    private ListView lv_tests, lv_all_tests;
+    private AutoCompleteTextView actv_search;
+    private EditText edt_add;
+    private ArrayAdapter<String> listAdapter;
+    private ProgressDialog progressDialog;
+    private ArrayList<String> masterDataString = new ArrayList<>();
+    private ArrayList<DataObj> checkList = new ArrayList<>();
+    private MultiSelectListAdapter multiSelectListAdapter;
+    private ArrayAdapter<String> actvAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.frag_preference_hc_service, container, false);
+        initProgress();
+        edt_add = v.findViewById(R.id.edt_add);
+        lv_tests = v.findViewById(R.id.lv_tests);
+        lv_all_tests = v.findViewById(R.id.lv_all_tests);
+        actv_search = v.findViewById(R.id.actv_search);
+        checkList = LaunchActivity.getSelectedTest();
+        if (null == checkList)
+            checkList = new ArrayList<>();
+        actv_search.setThreshold(1);
+        actv_search.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = (String) parent.getItemAtPosition(position);
+                DataObj dataObj = new DataObj();
+                dataObj.setName(value);
+                dataObj.setSelect(false);
+                if (!checkList.contains(dataObj)) {
+                    checkList.add(dataObj);
+                    multiSelectListAdapter.notifyDataSetChanged();
+                    actv_search.setText("");
+                } else {
+                    Toast.makeText(getActivity(), "Already selected", Toast.LENGTH_LONG).show();
+                }
+                new AppUtils().hideKeyBoard(getActivity());
+            }
+        });
+        actv_search.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_LEFT = 0;
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (actv_search.getRight() - actv_search.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        actv_search.setText("");
+                        return true;
+                    }
+                    if (event.getRawX() <= (20 + actv_search.getLeft() + actv_search.getCompoundDrawables()[DRAWABLE_LEFT].getBounds().width())) {
+                        //performSearch();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        multiSelectListAdapter = new MultiSelectListAdapter(getActivity(), checkList);
+        lv_tests.setAdapter(multiSelectListAdapter);
+        lv_all_tests.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DataObj dataObj = new DataObj();
+                dataObj.setName(masterDataString.get(position));
+                dataObj.setSelect(false);
+                if (!checkList.contains(dataObj)) {
+                    checkList.add(dataObj);
+                    multiSelectListAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        Button btn_add = v.findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.show();
+                MasterLabModel masterLabModel = new MasterLabModel();
+                masterLabModel.setMasterLabPresenter(PreferenceHCServiceFragment.this);
+                masterLabModel.add(UserUtils.getDeviceId(), UserUtils.getEmail(), UserUtils.getAuth(), new JsonMasterLab().
+                        setProductName(edt_add.getText().toString()).setProductShortName(edt_add.getText().toString()).setHealthCareService(getHealthCareEnum(getArguments().getInt("type"))));
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void masterLabUploadResponse(JsonResponse jsonResponse) {
+        dismissProgress();
+        if (Constants.SUCCESS == jsonResponse.getResponse()) {
+            masterDataString.add(edt_add.getText().toString());
+            listAdapter.notifyDataSetChanged();
+            actvAdapter.notifyDataSetChanged();
+            DataObj dataObj = new DataObj();
+            dataObj.setName(edt_add.getText().toString());
+            dataObj.setSelect(false);
+            checkList.add(dataObj);
+            multiSelectListAdapter.notifyDataSetChanged();
+            edt_add.setText("");
+            Toast.makeText(getActivity(), "Test updated Successfully", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getActivity(), "Failed to update test", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void responseErrorPresenter(ErrorEncounteredJson eej) {
+        dismissProgress();
+        new ErrorResponseHandler().processError(getActivity(), eej);
+    }
+
+    @Override
+    public void responseErrorPresenter(int errorCode) {
+        dismissProgress();
+        new ErrorResponseHandler().processFailureResponseCode(getActivity(), errorCode);
+    }
+
+    @Override
+    public void authenticationFailure() {
+        dismissProgress();
+        AppUtils.authenticationProcessing();
+    }
+
+    private void initProgress() {
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Updating data...");
+    }
+
+    protected void dismissProgress() {
+        if (null != progressDialog && progressDialog.isShowing())
+            progressDialog.dismiss();
+    }
+
+    public void setData(ArrayList<String> tempList) {
+        masterDataString = tempList;
+        listAdapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, masterDataString);
+        lv_all_tests.setAdapter(listAdapter);
+        actvAdapter = new ArrayAdapter<>(getActivity(), R.layout.spinner_item, masterDataString);
+        actv_search.setAdapter(actvAdapter);
+    }
+
+    private HealthCareServiceEnum getHealthCareEnum(int pos) {
+        switch (pos) {
+            case 0:
+                return HealthCareServiceEnum.MRI;
+            case 1:
+                return HealthCareServiceEnum.SCAN;
+            case 2:
+                return HealthCareServiceEnum.SONO;
+            case 3:
+                return HealthCareServiceEnum.XRAY;
+            case 4:
+                return HealthCareServiceEnum.PATH;
+            default:
+                return HealthCareServiceEnum.PATH;
+        }
+    }
+}
