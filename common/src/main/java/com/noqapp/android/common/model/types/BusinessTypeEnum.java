72480d63e6654b86089e76a9b1f2e9f03387700a
hth <hth@users.noreply.github.com>
2018-11-27 14:44:52 +0530

    removed unused enum; clean up

--- common/src/main/java/com/noqapp/android/common/model/types/BusinessTypeEnum.java
+++ common/src/main/java/com/noqapp/android/common/model/types/BusinessTypeEnum.java
@@ -21,9 +21,6 @@ public enum BusinessTypeEnum implements Serializable {
     DO("DO", "Hospital/Doctor", Q),
     HS("HS", "Health Care Services", O),
     PH("PH", "Pharmacy", O),                //Users cannot directly order these, as these have to be prescribed
-    RA("RA", "Radiology", O),               //Users cannot directly order these, as these have to be prescribed
-    PY("PY", "Physiotherapy", O),           //Users cannot directly order these, as these have to be prescribed
-    PT("PT", "Pathology", O),               //Users cannot directly order these, as these have to be prescribed
     PW("PW", "Place of Worship", Q),
     MU("MU", "Museum", Q),
     TA("TA", "Tourist Attraction", Q),
