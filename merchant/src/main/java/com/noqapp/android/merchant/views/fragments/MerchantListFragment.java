1cdb4105196228f2779828cb0a5c57039b6e007c
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2018-12-20 15:24:34 +0530

    Title: cleanup

--- merchant/src/main/java/com/noqapp/android/merchant/views/fragments/MerchantListFragment.java
+++ merchant/src/main/java/com/noqapp/android/merchant/views/fragments/MerchantListFragment.java
@@ -58,12 +58,7 @@ import java.util.List;
 public class MerchantListFragment extends Fragment implements TopicPresenter, FragmentCommunicator, AdapterCallback, SwipeRefreshLayout.OnRefreshListener {
 
     public static int selected_pos = 0;
-
-    public MerchantDetailFragment getMerchantDetailFragment() {
-        return merchantDetailFragment;
-    }
-
-    public MerchantDetailFragment merchantDetailFragment;
+    private MerchantDetailFragment merchantDetailFragment;
     private Handler timerHandler;
     private MerchantListAdapter adapter;
     private AutocompleteAdapter temp_adapter;
