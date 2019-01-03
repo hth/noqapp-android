929ae1d025cbcf77df2231c10f37060c757ccfe2
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-01-03 13:59:58 +0530

    Title: Enum description changes

--- common/src/main/java/com/noqapp/android/common/model/types/medical/DailyFrequencyEnum.java
+++ common/src/main/java/com/noqapp/android/common/model/types/medical/DailyFrequencyEnum.java
@@ -9,11 +9,13 @@ import java.util.List;
  * 8/17/18 5:31 PM
  */
 public enum DailyFrequencyEnum {
-    OD("OD", "1 time a day", 1),
-    TD("TD", "2 times a day", 2),
-    HD("HD", "3 times a day", 3),
-    FD("FD", "4 times a day", 4),
-    VD("VD", "5 times a day", 5);
+    OD("OD", "Once a day", 1),
+    TD("TD", "Twice a day", 2),
+    HD("HD", "Thrice a day", 3),
+    FD("FD", "Four times a day", 4),
+    VD("VD", "Five times a day", 5);
+
+
 
     private final String description;
     private final String name;
@@ -79,15 +81,15 @@ public enum DailyFrequencyEnum {
     public static String getValue(String input){
         try {
             switch (input) {
-                case "1 time a day":
+                case "Once a day":
                     return "OD";
-                case "2 times a day":
+                case "Twice a day":
                     return "TD";
-                case "3 times a day":
+                case "Thrice a day":
                     return "HD";
-                case "4 times a day":
+                case "Four times a day":
                     return "FD";
-                case "5 times a day":
+                case "Five times a day":
                     return "VD";
                 default:
                     return input;
