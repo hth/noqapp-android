ab8447250c0dccd22a045aaf1f85db12208c4cd7
hth <hth@users.noreply.github.com>
2019-01-11 08:14:27 +0530

    clean up

--- merchant/src/healthcare/java/com/noqapp/android/merchant/views/adapters/MedicalRecordAdapter.java
+++ merchant/src/healthcare/java/com/noqapp/android/merchant/views/adapters/MedicalRecordAdapter.java
@@ -68,20 +68,21 @@ public class MedicalRecordAdapter extends BaseAdapter {
         recordHolder.tv_daily_frequency.setText(medicalRecord.getDailyFrequency());
         recordHolder.tv_course.setText(medicalRecord.getCourse() + " days");
         recordHolder.tv_medicine_name.setText(medicalRecord.getName());
-        if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.CA.getDescription()))
+        if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.CA.getDescription())) {
             recordHolder.tv_medication.setCompoundDrawablesWithIntrinsicBounds(R.drawable.med_capsule, 0, 0, 0);
-        else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.TA.getDescription()))
+        } else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.TA.getDescription())) {
             recordHolder.tv_medication.setCompoundDrawablesWithIntrinsicBounds(R.drawable.med_tablet, 0, 0, 0);
-        else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.SY.getDescription()))
+        } else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.SY.getDescription())) {
             recordHolder.tv_medication.setCompoundDrawablesWithIntrinsicBounds(R.drawable.med_syrup, 0, 0, 0);
-        else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.IJ.getDescription()))
+        } else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.IJ.getDescription())) {
             recordHolder.tv_medication.setCompoundDrawablesWithIntrinsicBounds(R.drawable.med_injection, 0, 0, 0);
-        else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.LO.getDescription()))
+        } else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.LO.getDescription())) {
             recordHolder.tv_medication.setCompoundDrawablesWithIntrinsicBounds(R.drawable.med_lotion, 0, 0, 0);
-        else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.CR.getDescription()))
+        } else if (medicalRecord.getPharmacyCategory().equals(PharmacyCategoryEnum.CR.getDescription())) {
             recordHolder.tv_medication.setCompoundDrawablesWithIntrinsicBounds(R.drawable.med_cream, 0, 0, 0);
-        else
+        } else {
             recordHolder.tv_medication.setCompoundDrawables(null, null, null, null);
+        }
         return view;
     }
 
