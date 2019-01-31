f34a719f3b264cb465832f41c827134f0909755e
hth <hth@users.noreply.github.com>
2019-01-31 15:50:58 +0530

    Added API to flag data

--- merchant/src/main/java/com/noqapp/android/merchant/model/response/api/health/MasterLabService.java
+++ merchant/src/main/java/com/noqapp/android/merchant/model/response/api/health/MasterLabService.java
@@ -28,9 +28,33 @@ public interface MasterLabService {
             @Header("X-R-AUTH")
             String auth
     );
-    
+
+    @Deprecated
     @POST("api/m/h/lab/add.json")
     Call<JsonResponse> add(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth,
+
+            @Body
+            JsonMasterLab jsonMasterLab
+    );
+
+    /**
+     * Errors
+     * {@link javax.servlet.http.HttpServletResponse#SC_UNAUTHORIZED} - HTTP STATUS 401
+     * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#SEVERE}
+     */
+    @POST("api/m/h/lab/flag.json")
+    Call<JsonResponse> flag(
             @Header("X-R-DID")
             String did,
 
