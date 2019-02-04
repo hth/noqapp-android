5c7864686a65ff87f5f89da135e1bdf135166747
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-02-04 17:54:13 +0530

    Title - Merchant-Healthcare >> Dynamic image upload process added; Max image upload limit 25 set; Floating action button for add image introduce

---
+++ merchant/src/healthcare/java/com/noqapp/android/merchant/views/adapters/ImageUploadAdapter.java
@@ -0,0 +1,94 @@
+package com.noqapp.android.merchant.views.adapters;
+
+import com.noqapp.android.merchant.BuildConfig;
+import com.noqapp.android.merchant.R;
+
+import com.squareup.picasso.Picasso;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+
+public class ImageUploadAdapter extends RecyclerView.Adapter<ImageUploadAdapter.MyViewHolder> {
+    private final OnItemClickListener listener;
+    private List<String> imageUrls;
+    private Context context;
+    private String recordReferenceId;
+
+    public ImageUploadAdapter(List<String> data, Context context, String recordReferenceId, OnItemClickListener listener) {
+        this.imageUrls = data;
+        this.listener = listener;
+        this.context = context;
+        this.recordReferenceId = recordReferenceId;
+    }
+
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                           int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.rcv_item_upload_pic, parent, false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int listPosition) {
+        holder.iv_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != listener) {
+                    listener.imageDeleteClick(imageUrls.get(listPosition));
+                }
+            }
+        });
+        holder.iv_inlarge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != listener) {
+                    listener.imageEnlargeClick(imageUrls.get(listPosition));
+                }
+            }
+        });
+
+        if (!TextUtils.isEmpty(imageUrls.get(listPosition))) {
+            Picasso.with(context)
+                    .load(BuildConfig.AWSS3 + BuildConfig.MEDICAL_BUCKET + recordReferenceId + "/" + imageUrls.get(listPosition))
+                    .into(holder.iv_thumb);
+        } else {
+            Picasso.with(context).load(R.drawable.profile_blue).into(holder.iv_thumb);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageUrls.size();
+    }
+
+    public interface OnItemClickListener {
+        void imageEnlargeClick(String imageUrl);
+
+        void imageDeleteClick(String imageUrl);
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        private ImageView iv_thumb;
+        private ImageView iv_delete;
+        private ImageView iv_inlarge;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            this.iv_thumb = itemView.findViewById(R.id.iv_thumb);
+            this.iv_delete = itemView.findViewById(R.id.iv_delete);
+            this.iv_inlarge = itemView.findViewById(R.id.iv_inlarge);
+        }
+    }
+
+}
