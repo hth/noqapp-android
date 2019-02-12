e15af85c4e8fa6ac84b8b0645e217add91891853
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-02-12 14:18:03 +0530

    Title: Client - Feed screen UI changes

--- client/src/main/java/com/noqapp/android/client/views/activities/FeedActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/FeedActivity.java
@@ -10,54 +10,58 @@ import com.squareup.picasso.Picasso;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class FeedActivity extends BaseActivity {
+public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
-        initActionsViews(true);
 
-        tv_toolbar_title.setText("");
-        ImageView iv_bg = findViewById(R.id.iv_bg);
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mToolbar.setNavigationIcon(R.drawable.ic_back_new);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setTitle(" ");
+
+
+        ImageView iv_bg = findViewById(R.id.expandedImage);
         TextView tv_details = findViewById(R.id.tv_details);
         TextView tv_title = findViewById(R.id.tv_title);
         final JsonFeed jsonFeed = (JsonFeed) getIntent().getSerializableExtra("object");
         tv_title.setText(jsonFeed.getTitle());
-
         RelativeLayout rl_author = findViewById(R.id.rl_author);
-        // View view_separator = findViewById(R.id.view_separator);
-
         if (TextUtils.isEmpty(jsonFeed.getAuthor())) {
             rl_author.setVisibility(View.GONE);
-            // view_separator.setVisibility(View.GONE);
         } else {
             rl_author.setVisibility(View.VISIBLE);
-            //  view_separator.setVisibility(View.VISIBLE);
             ImageView iv_main = findViewById(R.id.iv_main);
             TextView tv_author_name = findViewById(R.id.tv_author_name);
             TextView tv_author_profession = findViewById(R.id.tv_author_profession);
             Picasso.with(this).load(jsonFeed.getAuthorThumbnail()).into(iv_main);
             tv_author_name.setText(jsonFeed.getAuthor());
             tv_author_profession.setText(jsonFeed.getProfession());
-            rl_author.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-//                    Intent intent = new Intent(FeedActivity.this, ManagerProfileActivity.class);
-//                    intent.putExtra("webProfileId", jsonFeed.getWebProfileId());
-//                    intent.putExtra("managerName", jsonFeed.getAuthor());
-//                    intent.putExtra("managerImage", jsonFeed.getAuthorThumbnail());
-//                    intent.putExtra("bizCategoryId", MedicalDepartmentEnum.PHY.getName());
-//                    startActivity(intent);
-                }
-            });
+
         }
 
         Picasso.with(this).load(jsonFeed.getImageUrl()).into(iv_bg);
@@ -74,5 +78,25 @@ public class FeedActivity extends BaseActivity {
                     .putContentType(jsonFeed.getContentType())
                     .putContentId("feed-dec-18"));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_favourite) {
+            Toast.makeText(FeedActivity.this, "Hello", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
