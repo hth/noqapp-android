4c2719a0b93a4406eab19a8e1acd53ed87066fa3
hth <hth@users.noreply.github.com>
2019-03-05 13:33:34 +0530

    updated payment mode

--- common/src/main/java/com/noqapp/android/common/model/types/order/PaymentModeEnum.java
+++ common/src/main/java/com/noqapp/android/common/model/types/order/PaymentModeEnum.java
@@ -6,10 +6,23 @@ package com.noqapp.android.common.model.types.order;
 
 public enum PaymentModeEnum {
     CA("CA", "Cash"),
-    CC("CC", "Credit Card"),
     DC("DC", "Debit Card"),
-    PT("PT", "Paytm"),
-    UP("UP", "UPI");
+    CC("CC", "Credit Card"),
+    CCE("CCE", "Credit Card EMI"),
+    NTB("NTB", "Internet Banking"),
+    UPI("UPI", "UPI"),
+    PAL("PAL", "Paypal"),
+    PPE("PPE", "PhonePe"),
+    PTM("PTM", "Paytm"),
+    AMZ("AMZ", "AmazonPay"),
+    AIR("AIR", "Airtel Money Wallet"),
+    FCH("FCH", "Freecharge Wallet\n"),
+    MKK("MKK", "MobiKwik Wallet"),
+    OLA("OLA", "Ola Wallet"),
+    JIO("JIO", "JioMoney Wallet"),
+    ZST("ZST", "ZestMoney"),
+    INS("INS", "AmazonPay"),
+    LPY("LPY", "LazyPay");
 
     private final String description;
     private final String name;
