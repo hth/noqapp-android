96bbabcd8a5c9640011db4dd5e0e4f4626f80c40
hth <hth@users.noreply.github.com>
2019-03-08 15:50:45 +0530

    clean up

--- client/src/test/java/com/noqapp/android/client/model/DeviceApiCallITest.java
+++ client/src/test/java/com/noqapp/android/client/model/DeviceApiCallITest.java
@@ -15,6 +15,8 @@ import com.noqapp.android.common.presenter.DeviceRegisterPresenter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import android.util.Log;
 import retrofit2.Call;
@@ -25,64 +27,12 @@ import java.util.concurrent.Callable;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DeviceApiCallITest extends ITest {
 
-    private AppBlacklistPresenter appBlacklistPresenter;
-    private DeviceRegisterPresenter deviceRegisterPresenter;
+    @Mock private AppBlacklistPresenter appBlacklistPresenter;
+    @Mock private DeviceRegisterPresenter deviceRegisterPresenter;
 
     @BeforeEach
     void setUp() {
-        this.appBlacklistPresenter = new AppBlacklistPresenter() {
-            @Override
-            public void appBlacklistError(ErrorEncounteredJson eej) {
-
-            }
-
-            @Override
-            public void appBlacklistResponse(JsonLatestAppVersion jsonLatestAppVersion) {
-
-            }
-
-            @Override
-            public void responseErrorPresenter(ErrorEncounteredJson eej) {
-
-            }
-
-            @Override
-            public void responseErrorPresenter(int errorCode) {
-
-            }
-
-            @Override
-            public void authenticationFailure() {
-
-            }
-        };
-
-        this.deviceRegisterPresenter = new DeviceRegisterPresenter() {
-            @Override
-            public void deviceRegisterError() {
-
-            }
-
-            @Override
-            public void deviceRegisterResponse(DeviceRegistered deviceRegistered) {
-
-            }
-
-            @Override
-            public void responseErrorPresenter(ErrorEncounteredJson eej) {
-
-            }
-
-            @Override
-            public void responseErrorPresenter(int errorCode) {
-
-            }
-
-            @Override
-            public void authenticationFailure() {
-
-            }
-        };
+        MockitoAnnotations.initMocks(this);
 
         this.deviceApiCall.setAppBlacklistPresenter(appBlacklistPresenter);
         this.deviceApiCall.setDeviceRegisterPresenter(deviceRegisterPresenter);
@@ -104,11 +54,11 @@ class DeviceApiCallITest extends ITest {
     void isSupportedAppVersion() {
         String did = UUID.randomUUID().toString();
         this.deviceApiCall.isSupportedAppVersion(did);
-        await().atMost(1, MINUTES).pollInterval(5, SECONDS).until(awaitUntilPurchaseOrderIsPlaced());
+        await().atMost(30, SECONDS).pollInterval(5, SECONDS).until(awaitUntilResponseFromServer());
         assertEquals("1.2.230", deviceApiCall.getJsonLatestAppVersion().getLatestAppVersion());
     }
 
-    private Callable<Boolean> awaitUntilPurchaseOrderIsPlaced() {
+    private Callable<Boolean> awaitUntilResponseFromServer() {
         return () -> this.deviceApiCall.isResponseReceived();
     }
 }
