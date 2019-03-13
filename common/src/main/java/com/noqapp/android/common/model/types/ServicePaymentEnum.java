48d1d74ba7d939e0127d09fcfe55752e9be30272
hth <hth@users.noreply.github.com>
2019-03-13 15:13:31 +0530

    Updated enum value

--- common/src/main/java/com/noqapp/android/common/model/types/ServicePaymentEnum.java
+++ common/src/main/java/com/noqapp/android/common/model/types/ServicePaymentEnum.java
@@ -5,9 +5,9 @@ package com.noqapp.android.common.model.types;
  * 2019-03-13 11:45
  */
 public enum ServicePaymentEnum {
-    N("N", "Not Required"),
-    R("R", "Required"),
-    B("B", "Both");
+    N("N", "Payment Not Required"),
+    R("R", "Payment Required"),
+    O("O", "Payment Optional");
 
     private final String name;
     private final String description;
