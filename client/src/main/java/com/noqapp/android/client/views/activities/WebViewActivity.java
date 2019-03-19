ef4116ec380130ac9547bb62db9f4096f9aa1c06
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-03-19 10:55:11 +0530

    Title: Client - actual pdf view in medical detail screen

--- client/src/main/java/com/noqapp/android/client/views/activities/WebViewActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/WebViewActivity.java
@@ -50,9 +50,10 @@ public class WebViewActivity extends BaseActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setVerticalScrollBarEnabled(true);
         webView.setHorizontalScrollBarEnabled(true);
-        webView.getSettings().setUseWideViewPort(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
         webView.getSettings().setRenderPriority(RenderPriority.HIGH);
         webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
         webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
