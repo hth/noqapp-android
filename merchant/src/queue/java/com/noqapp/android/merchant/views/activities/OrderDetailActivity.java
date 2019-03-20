72abcf345b7047acc052109e8ed1ee3947088319
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-03-20 10:51:56 +0530

    Title : Merchant Store - payment flow set for store order

--- merchant/src/queue/java/com/noqapp/android/merchant/views/activities/OrderDetailActivity.java
+++ merchant/src/queue/java/com/noqapp/android/merchant/views/activities/OrderDetailActivity.java
@@ -5,6 +5,7 @@ import com.noqapp.android.common.beans.ErrorEncounteredJson;
 import com.noqapp.android.common.beans.store.JsonPurchaseOrder;
 import com.noqapp.android.common.beans.store.JsonPurchaseOrderList;
 import com.noqapp.android.common.beans.store.JsonPurchaseOrderProduct;
+import com.noqapp.android.common.model.types.order.PaymentModeEnum;
 import com.noqapp.android.common.model.types.order.PaymentStatusEnum;
 import com.noqapp.android.merchant.R;
 import com.noqapp.android.merchant.utils.AppUtils;
@@ -46,10 +47,16 @@ public class OrderDetailActivity extends AppCompatActivity implements PaymentPro
     private boolean isProductWithoutPrice = false;
     private TextView tv_cost;
     private String currencySymbol;
-    private TextView tv_paid_amount_value, tv_remaining_amount_value;
+    private TextView tv_paid_amount_value, tv_remaining_amount_value, tv_notes;
     private Spinner sp_payment_mode;
     private String[] payment_modes = {"Cash", "Cheque", "Credit Card", "Debit Card", "Internet Banking", "Paytm"};
+    private PaymentModeEnum[] payment_modes_enum = {PaymentModeEnum.CA, PaymentModeEnum.UPI, PaymentModeEnum.CC, PaymentModeEnum.DC, PaymentModeEnum.NTB, PaymentModeEnum.PTM};
     private Button btn_update_price;
+    private CardView cv_notes;
+    private EditText edt_amount;
+    private RelativeLayout rl_payment;
+    private TextView tv_payment_mode, tv_payment_status, tv_address;
+    private Button btn_pay_partial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,36 +92,106 @@ public class OrderDetailActivity extends AppCompatActivity implements PaymentPro
 
         ListView listview = findViewById(R.id.listview);
         TextView tv_item_count = findViewById(R.id.tv_item_count);
-        TextView tv_payment_mode = findViewById(R.id.tv_payment_mode);
-        TextView tv_payment_status = findViewById(R.id.tv_payment_status);
-        TextView tv_address = findViewById(R.id.tv_address);
+        tv_payment_mode = findViewById(R.id.tv_payment_mode);
+        tv_payment_status = findViewById(R.id.tv_payment_status);
+        tv_address = findViewById(R.id.tv_address);
         btn_update_price = findViewById(R.id.btn_update_price);
         tv_cost = findViewById(R.id.tv_cost);
         sp_payment_mode = findViewById(R.id.sp_payment_mode);
         ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, payment_modes);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp_payment_mode.setAdapter(aa);
-        try {
-            sp_payment_mode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        sp_payment_mode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                try {
                     ((TextView) view).setTextColor(Color.BLACK); //Change selected text color
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+            }
 
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        TextView tv_notes = findViewById(R.id.tv_notes);
+            }
+        });
+
+        tv_notes = findViewById(R.id.tv_notes);
         tv_remaining_amount_value = findViewById(R.id.tv_remaining_amount_value);
         tv_paid_amount_value = findViewById(R.id.tv_paid_amount_value);
-        CardView cv_notes = findViewById(R.id.cv_notes);
-        EditText edt_amount = findViewById(R.id.edt_amount);
-        RelativeLayout rl_payment = findViewById(R.id.rl_payment);
+        cv_notes = findViewById(R.id.cv_notes);
+        edt_amount = findViewById(R.id.edt_amount);
+        rl_payment = findViewById(R.id.rl_payment);
+
+        Button btn_pay_now = findViewById(R.id.btn_pay_now);
+        btn_pay_partial = findViewById(R.id.btn_pay_partial);
+        initProgress();
+        updateUI();
+        btn_pay_partial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isProductWithoutPrice) {
+                    Toast.makeText(OrderDetailActivity.this, "Some product having 0 price. Please set price to them", Toast.LENGTH_LONG).show();
+                } else {
+                    if (TextUtils.isEmpty(edt_amount.getText().toString())) {
+                        Toast.makeText(OrderDetailActivity.this, "Please enter amount to pay.", Toast.LENGTH_LONG).show();
+                    } else {
+                        if (Double.parseDouble(edt_amount.getText().toString()) * 100 > Double.parseDouble(jsonPurchaseOrder.getOrderPrice())) {
+                            Toast.makeText(OrderDetailActivity.this, "Please enter amount less or equal to order amount.", Toast.LENGTH_LONG).show();
+                        } else {
+                            progressDialog.show();
+                            progressDialog.setMessage("Starting payment..");
+                            jsonPurchaseOrder.setPaymentMode(payment_modes_enum[sp_payment_mode.getSelectedItemPosition()]);
+                            jsonPurchaseOrder.setPartialPayment(String.valueOf(Double.parseDouble(edt_amount.getText().toString()) * 100));
+                            PurchaseOrderApiCalls purchaseOrderApiCalls = new PurchaseOrderApiCalls();
+                            purchaseOrderApiCalls.setPaymentProcessPresenter(OrderDetailActivity.this);
+                            purchaseOrderApiCalls.partialPayment(UserUtils.getDeviceId(), UserUtils.getEmail(), UserUtils.getAuth(), jsonPurchaseOrder);
+                        }
+                    }
+                }
+            }
+        });
+        btn_update_price.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isProductWithoutPrice) {
+                    Toast.makeText(OrderDetailActivity.this, "Some product having 0 price. Please set price to them", Toast.LENGTH_LONG).show();
+                } else {
+                    progressDialog.show();
+                    progressDialog.setMessage("Updating price..");
+                    PurchaseOrderApiCalls purchaseOrderApiCalls = new PurchaseOrderApiCalls();
+                    purchaseOrderApiCalls.setModifyOrderPresenter(OrderDetailActivity.this);
+                    purchaseOrderApiCalls.modify(UserUtils.getDeviceId(), UserUtils.getEmail(), UserUtils.getAuth(), jsonPurchaseOrder);
+                }
+            }
+        });
+
+        btn_pay_now.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isProductWithoutPrice) {
+                    Toast.makeText(OrderDetailActivity.this, "Some product having 0 price. Please set price to them", Toast.LENGTH_LONG).show();
+                } else {
+                    progressDialog.show();
+                    progressDialog.setMessage("Starting payment..");
+                    jsonPurchaseOrder.setPaymentMode(payment_modes_enum[sp_payment_mode.getSelectedItemPosition()]);
+                    jsonPurchaseOrder.setPartialPayment(jsonPurchaseOrder.getOrderPrice());
+                    PurchaseOrderApiCalls purchaseOrderApiCalls = new PurchaseOrderApiCalls();
+                    purchaseOrderApiCalls.setPaymentProcessPresenter(OrderDetailActivity.this);
+                    purchaseOrderApiCalls.setPurchaseOrderPresenter(OrderDetailActivity.this);
+                    purchaseOrderApiCalls.cashPayment(UserUtils.getDeviceId(), UserUtils.getEmail(), UserUtils.getAuth(), jsonPurchaseOrder);
+                }
+
+            }
+        });
+        tv_item_count.setText("Total Items: (" + jsonPurchaseOrder.getPurchaseOrderProducts().size() + ")");
+        OrderItemAdapter adapter = new OrderItemAdapter(this, jsonPurchaseOrder.getPurchaseOrderProducts(), currencySymbol, this);
+        listview.setAdapter(adapter);
+    }
+
+
+    private void updateUI() {
         tv_notes.setText("Additional Notes: " + jsonPurchaseOrder.getAdditionalNote());
         cv_notes.setVisibility(TextUtils.isEmpty(jsonPurchaseOrder.getAdditionalNote()) ? View.GONE : View.VISIBLE);
         tv_address.setText(Html.fromHtml(jsonPurchaseOrder.getDeliveryAddress()));
@@ -130,8 +207,6 @@ public class OrderDetailActivity extends AppCompatActivity implements PaymentPro
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Button btn_pay_now = findViewById(R.id.btn_pay_now);
-        Button btn_pay_partial = findViewById(R.id.btn_pay_partial);
         if (PaymentStatusEnum.PP == jsonPurchaseOrder.getPaymentStatus() ||
                 PaymentStatusEnum.MP == jsonPurchaseOrder.getPaymentStatus()) {
             if (isProductWithoutPrice) {
@@ -165,71 +240,6 @@ public class OrderDetailActivity extends AppCompatActivity implements PaymentPro
         } catch (Exception e) {
             tv_cost.setText(currencySymbol + " " + String.valueOf(0 / 100));
         }
-        initProgress();
-
-        btn_pay_partial.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isProductWithoutPrice) {
-                    Toast.makeText(OrderDetailActivity.this, "Some product having 0 price. Please set price to them", Toast.LENGTH_LONG).show();
-                } else {
-                    if (TextUtils.isEmpty(edt_amount.getText().toString())) {
-                        Toast.makeText(OrderDetailActivity.this, "Please enter amount to pay.", Toast.LENGTH_LONG).show();
-                    } else {
-                        if (Double.parseDouble(edt_amount.getText().toString()) * 100 > Double.parseDouble(jsonPurchaseOrder.getOrderPrice())) {
-                            Toast.makeText(OrderDetailActivity.this, "Please enter amount less or equal to order amount.", Toast.LENGTH_LONG).show();
-                        } else {
-                            progressDialog.show();
-                            progressDialog.setMessage("Starting payment..");
-                            //jsonPurchaseOrder.setPaymentMode(PaymentModeEnum.CA); //not required here
-                            jsonPurchaseOrder.setPartialPayment(String.valueOf(Double.parseDouble(edt_amount.getText().toString()) * 100));
-                            PurchaseOrderApiCalls purchaseOrderApiCalls = new PurchaseOrderApiCalls();
-                            purchaseOrderApiCalls.setPaymentProcessPresenter(OrderDetailActivity.this);
-                            purchaseOrderApiCalls.setPurchaseOrderPresenter(OrderDetailActivity.this);
-                            purchaseOrderApiCalls.partialPayment(UserUtils.getDeviceId(), UserUtils.getEmail(), UserUtils.getAuth(), jsonPurchaseOrder);
-                        }
-                    }
-                }
-            }
-        });
-        btn_update_price.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isProductWithoutPrice) {
-                    Toast.makeText(OrderDetailActivity.this, "Some product having 0 price. Please set price to them", Toast.LENGTH_LONG).show();
-                } else {
-                    progressDialog.show();
-                    progressDialog.setMessage("Updating price..");
-                    //jsonPurchaseOrder.setPaymentMode(PaymentModeEnum.CA); //not required here
-                    //jsonPurchaseOrder.setPartialPayment(String.valueOf(Double.parseDouble(edt_amount.getText().toString()) * 100));
-                    PurchaseOrderApiCalls purchaseOrderApiCalls = new PurchaseOrderApiCalls();
-                    purchaseOrderApiCalls.setModifyOrderPresenter(OrderDetailActivity.this);
-                    purchaseOrderApiCalls.modify(UserUtils.getDeviceId(), UserUtils.getEmail(), UserUtils.getAuth(), jsonPurchaseOrder);
-                }
-            }
-        });
-
-        btn_pay_now.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isProductWithoutPrice) {
-                    Toast.makeText(OrderDetailActivity.this, "Some product having 0 price. Please set price to them", Toast.LENGTH_LONG).show();
-                } else {
-                    progressDialog.show();
-                    progressDialog.setMessage("Starting payment..");
-                    //jsonPurchaseOrder.setPaymentMode(PaymentModeEnum.CA); //not required here
-                    jsonPurchaseOrder.setPartialPayment(jsonPurchaseOrder.getOrderPrice());
-                    PurchaseOrderApiCalls purchaseOrderApiCalls = new PurchaseOrderApiCalls();
-                    purchaseOrderApiCalls.setPaymentProcessPresenter(OrderDetailActivity.this);
-                    purchaseOrderApiCalls.setPurchaseOrderPresenter(OrderDetailActivity.this);
-                    purchaseOrderApiCalls.cashPayment(UserUtils.getDeviceId(), UserUtils.getEmail(), UserUtils.getAuth(), jsonPurchaseOrder);
-                }
-
-            }
-        });
-        tv_item_count.setText("Total Items: (" + jsonPurchaseOrder.getPurchaseOrderProducts().size() + ")");
-        OrderItemAdapter adapter = new OrderItemAdapter(this, jsonPurchaseOrder.getPurchaseOrderProducts(), currencySymbol, this);
-        listview.setAdapter(adapter);
     }
 
     public void updateProductPriceList(JsonPurchaseOrderProduct jpop, int pos) {
@@ -317,6 +327,8 @@ public class OrderDetailActivity extends AppCompatActivity implements PaymentPro
         if (null != jsonPurchaseOrder) {
             if (jsonPurchaseOrder.getPaymentStatus() == PaymentStatusEnum.PA ||
                     jsonPurchaseOrder.getPaymentStatus() == PaymentStatusEnum.MP) {
+                this.jsonPurchaseOrder = jsonPurchaseOrder;
+                updateUI();
                 Toast.makeText(OrderDetailActivity.this, "Payment updated successfully", Toast.LENGTH_LONG).show();
                 if (null != BaseLaunchActivity.merchantListFragment) {
                     BaseLaunchActivity.merchantListFragment.onRefresh();
@@ -346,6 +358,8 @@ public class OrderDetailActivity extends AppCompatActivity implements PaymentPro
     public void modifyOrderResponse(JsonPurchaseOrder jsonPurchaseOrder) {
         dismissProgress();
         if (null != jsonPurchaseOrder) {
+            this.jsonPurchaseOrder = jsonPurchaseOrder;
+            updateUI();
             Log.v("modify order data:", jsonPurchaseOrder.toString());
             if (null != BaseLaunchActivity.merchantListFragment) {
                 BaseLaunchActivity.merchantListFragment.onRefresh();
