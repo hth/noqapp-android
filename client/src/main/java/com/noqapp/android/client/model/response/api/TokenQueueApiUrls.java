7d8df9f910a979422642a866b21243678341e7d4
hth <hth@users.noreply.github.com>
2019-03-26 15:55:32 +0530

    Api to skip payment for joining queue

--- client/src/main/java/com/noqapp/android/client/model/response/api/TokenQueueApiUrls.java
+++ client/src/main/java/com/noqapp/android/client/model/response/api/TokenQueueApiUrls.java
@@ -172,6 +172,30 @@ public interface TokenQueueApiUrls {
     /**
      * Errors
      * {@link javax.servlet.http.HttpServletResponse#SC_UNAUTHORIZED} - HTTP STATUS 401
+     * {@link javax.servlet.http.HttpServletResponse#SC_NOT_FOUND} - HTTP STATUS 404
+     * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#SEVERE}
+     */
+    @POST("api/c/token/skipPayBeforeQueue.json")
+    Call<JsonToken> skipPayBeforeQueue(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth,
+
+            @Body
+            JoinQueue joinQueue
+    );
+
+    /**
+     * Errors
+     * {@link javax.servlet.http.HttpServletResponse#SC_UNAUTHORIZED} - HTTP STATUS 401
      * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#PURCHASE_ORDER_NOT_FOUND}
      * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#TRANSACTION_GATEWAY_DEFAULT}
      * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#QUEUE_JOIN_PAYMENT_FAILED}
