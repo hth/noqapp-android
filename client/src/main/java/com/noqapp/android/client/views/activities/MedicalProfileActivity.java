406d0251dad247c2da676c4af7f3e59d2b4826a0
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2019-04-04 18:31:35 +0530

    Title : Client -  Patient Medical profile added

---
+++ client/src/main/java/com/noqapp/android/client/views/activities/MedicalProfileActivity.java
@@ -0,0 +1,18 @@
+package com.noqapp.android.client.views.activities;
+
+import android.os.Bundle;
+
+import com.noqapp.android.client.R;
+
+public class MedicalProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_profile);
+
+
+    }
+
+
+}
