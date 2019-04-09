e0bd688dad2ceb1c9d8bb75a803d424cae86d5d6
hth <hth@users.noreply.github.com>
2019-04-09 17:16:49 +0530

    Added Payment Refund

--- common/src/main/java/com/noqapp/android/common/model/types/order/PaymentStatusEnum.java
+++ common/src/main/java/com/noqapp/android/common/model/types/order/PaymentStatusEnum.java
@@ -10,7 +10,8 @@ public enum PaymentStatusEnum {
     PA("PA", "Paid"),
     PF("PF", "Payment Fail"),
     FP("FP", "Flagged Payment"),
-    PR("PC", "Payment Refund");
+    PC("PC", "Payment Cancelled"),
+    PR("PR", "Payment Refund");
 
     private final String description;
     private final String name;
