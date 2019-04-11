75a7c9cb348c2c1811320253276e1feec90ec53d
hth <hth@users.noreply.github.com>
2019-04-11 22:27:45 +0530

    formatting

--- merchant/src/main/java/com/noqapp/android/merchant/views/activities/MyApplication.java
+++ merchant/src/main/java/com/noqapp/android/merchant/views/activities/MyApplication.java
@@ -2,6 +2,8 @@ package com.noqapp.android.merchant.views.activities;
 
 import com.noqapp.android.common.utils.FontsOverride;
 
+import org.apache.commons.lang3.StringUtils;
+
 import android.app.Application;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -13,7 +15,6 @@ import java.util.Locale;
 /**
  * Created by chandra on 5/20/17.
  */
-
 public class MyApplication extends Application {
     private static final String LOG_TAG = "Application";
 
@@ -39,30 +40,26 @@ public class MyApplication extends Application {
     private Locale getLocaleFromPref() {
         Locale locale = Locale.getDefault();
         String language = getPreferences().getString("pref_language", "");
-        if (!language.equals("")) {
+        if (StringUtils.isNotBlank(language)) {
             locale = new Locale(language);
             Locale.setDefault(locale);
         }
         return locale;
     }
 
-    private void overwriteConfigurationLocale(Configuration config,
-                                              Locale locale) {
+    private void overwriteConfigurationLocale(Configuration config, Locale locale) {
         config.locale = locale;
-        getBaseContext().getResources().updateConfiguration(config,
-                getBaseContext().getResources().getDisplayMetrics());
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
     }
 
     public void setLocale() {
         Locale locale = getLocaleFromPref();
         Log.d(LOG_TAG, "Set locale to " + locale);
-        Configuration config = getBaseContext().getResources()
-                .getConfiguration();
+        Configuration config = getBaseContext().getResources().getConfiguration();
         overwriteConfigurationLocale(config, locale);
     }
 
     private SharedPreferences getPreferences() {
-        return PreferenceManager
-                .getDefaultSharedPreferences(getApplicationContext());
+        return PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
     }
 }
