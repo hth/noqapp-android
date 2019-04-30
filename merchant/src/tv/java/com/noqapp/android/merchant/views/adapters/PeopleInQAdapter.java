97926736a2ce10edbfcc47c02c6e4c6ce2338c15
hth <hth@users.noreply.github.com>
2019-04-30 22:24:00 +0530

    Formatting

--- merchant/src/tv/java/com/noqapp/android/merchant/views/adapters/PeopleInQAdapter.java
+++ merchant/src/tv/java/com/noqapp/android/merchant/views/adapters/PeopleInQAdapter.java
@@ -1,6 +1,5 @@
 package com.noqapp.android.merchant.views.adapters;
 
-
 import com.noqapp.android.common.model.types.QueueStatusEnum;
 import com.noqapp.android.merchant.presenter.beans.JsonDataVisibility;
 import com.noqapp.android.merchant.presenter.beans.JsonPaymentPermission;
@@ -11,7 +10,6 @@ import android.content.Context;
 import java.util.List;
 
 public class PeopleInQAdapter extends BasePeopleInQAdapter {
-
 
     public PeopleInQAdapter(
             List<JsonQueuedPerson> data,
