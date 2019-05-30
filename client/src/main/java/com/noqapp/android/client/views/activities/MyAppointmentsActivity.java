23182ab4b7ef06bc5ef92eb8dee8119a6ee0738a
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2019-05-30 18:46:09 +0530

    Title : Client - Tab based screen created for future/past appointment ; Seperate screen created for future & past appointment

--- client/src/main/java/com/noqapp/android/client/views/activities/MyAppointmentsActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/MyAppointmentsActivity.java
@@ -1,124 +1,62 @@
 package com.noqapp.android.client.views.activities;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.RelativeLayout;
 
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 import com.noqapp.android.client.R;
-import com.noqapp.android.client.model.AppointmentApiCalls;
-import com.noqapp.android.client.utils.AppUtilities;
-import com.noqapp.android.client.utils.ErrorResponseHandler;
-import com.noqapp.android.client.utils.IBConstant;
-import com.noqapp.android.client.utils.ShowAlertInformation;
-import com.noqapp.android.client.utils.UserUtils;
-import com.noqapp.android.client.views.adapters.MyAppointmentAdapter;
-import com.noqapp.android.common.beans.ErrorEncounteredJson;
-import com.noqapp.android.common.beans.JsonResponse;
-import com.noqapp.android.common.beans.JsonSchedule;
-import com.noqapp.android.common.beans.JsonScheduleList;
-import com.noqapp.android.common.presenter.AppointmentPresenter;
+import com.noqapp.android.client.views.adapters.TabViewPagerAdapter;
+import com.noqapp.android.client.views.fragments.FutureAppointmentFragment;
+import com.noqapp.android.client.views.fragments.PastAppointmentFragment;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MyAppointmentsActivity extends BaseActivity {
 
-public class MyAppointmentsActivity extends BaseActivity implements AppointmentPresenter,
-        MyAppointmentAdapter.OnItemClickListener {
-    private RecyclerView rcv_appointments;
-    private RelativeLayout rl_empty;
-    private List<JsonSchedule> jsonSchedules = new ArrayList<>();
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private LoadTabs loadTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upcoming_appointments);
-        initActionsViews(true);
-        tv_toolbar_title.setText(getString(R.string.screen_upcoming_appointment));
-        rcv_appointments = findViewById(R.id.rcv_appointments);
-        rl_empty = findViewById(R.id.rl_empty);
-        rcv_appointments.setHasFixedSize(true);
-        rcv_appointments.setLayoutManager(new LinearLayoutManager(this,
-                RecyclerView.VERTICAL, false));
-        rcv_appointments.setItemAnimator(new DefaultItemAnimator());
-        if (jsonSchedules.size() <= 0) {
-            rcv_appointments.setVisibility(View.GONE);
-            rl_empty.setVisibility(View.VISIBLE);
-        } else {
-            rcv_appointments.setVisibility(View.VISIBLE);
-            rl_empty.setVisibility(View.GONE);
-        }
-        if (LaunchActivity.getLaunchActivity().isOnline()) {
-            progressDialog.setMessage("Fetching appointments...");
-            progressDialog.show();
-            AppointmentApiCalls appointmentApiCalls = new AppointmentApiCalls();
-            appointmentApiCalls.setAppointmentPresenter(this);
-            appointmentApiCalls.allAppointments(UserUtils.getDeviceId(),
-                    UserUtils.getEmail(),
-                    UserUtils.getAuth());
-        } else {
-            ShowAlertInformation.showNetworkDialog(this);
+        setContentView(R.layout.activity_tab);
+        initActionsViews(false);
+        tv_toolbar_title.setText(getString(R.string.my_appointments));
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tabs);
+        loadTabs = new LoadTabs();
+        loadTabs.execute();
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        TabViewPagerAdapter adapter = new TabViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FutureAppointmentFragment(), "Future");
+        adapter.addFragment(new PastAppointmentFragment(), "Past");
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != loadTabs) {
+            loadTabs.cancel(true);
         }
     }
 
-    @Override
-    public void appointmentResponse(JsonScheduleList jsonScheduleList) {
-        Log.e("all appointments", jsonScheduleList.toString());
-        jsonSchedules = jsonScheduleList.getJsonSchedules();
-        if (jsonSchedules.size() <= 0) {
-            rcv_appointments.setVisibility(View.GONE);
-            rl_empty.setVisibility(View.VISIBLE);
-        } else {
-            rcv_appointments.setVisibility(View.VISIBLE);
-            rl_empty.setVisibility(View.GONE);
+    private class LoadTabs extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
         }
-        MyAppointmentAdapter appointmentListAdapter = new MyAppointmentAdapter(
-                jsonSchedules, this, this);
-        rcv_appointments.setAdapter(appointmentListAdapter);
-        dismissProgress();
+
+        protected void onPostExecute(String result) {
+            try {
+                setupViewPager(viewPager);
+                tabLayout.setupWithViewPager(viewPager);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-    @Override
-    public void appointmentBookingResponse(JsonSchedule jsonSchedule) {
-        dismissProgress();
-    }
-
-    @Override
-    public void appointmentCancelResponse(JsonResponse jsonResponse) {
-        dismissProgress();
-    }
-
-    @Override
-    public void authenticationFailure() {
-        dismissProgress();
-        AppUtilities.authenticationProcessing(this);
-    }
-
-    @Override
-    public void responseErrorPresenter(ErrorEncounteredJson eej) {
-        dismissProgress();
-        if (null != eej)
-            new ErrorResponseHandler().processError(this, eej);
-    }
-
-    @Override
-    public void responseErrorPresenter(int errorCode) {
-        dismissProgress();
-        new ErrorResponseHandler().processFailureResponseCode(this, errorCode);
-    }
-
-
-    @Override
-    public void appointmentDetails(JsonSchedule jsonSchedule) {
-        Intent intent = new Intent(this, AppointmentBookingDetailActivity.class);
-        intent.putExtra(IBConstant.KEY_DATA_OBJECT, jsonSchedule);
-        intent.putExtra(IBConstant.KEY_FROM_LIST, true);
-        startActivity(intent);
-    }
-
-
 }
---
+++ client/src/main/java/com/noqapp/android/client/views/fragments/PastAppointmentFragment.java
@@ -0,0 +1,140 @@
+package com.noqapp.android.client.views.fragments;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.noqapp.android.client.R;
+import com.noqapp.android.client.model.AppointmentApiCalls;
+import com.noqapp.android.client.utils.AppUtilities;
+import com.noqapp.android.client.utils.ErrorResponseHandler;
+import com.noqapp.android.client.utils.IBConstant;
+import com.noqapp.android.client.utils.ShowAlertInformation;
+import com.noqapp.android.client.utils.UserUtils;
+import com.noqapp.android.client.views.activities.AppointmentBookingDetailActivity;
+import com.noqapp.android.client.views.activities.LaunchActivity;
+import com.noqapp.android.client.views.adapters.MyAppointmentAdapter;
+import com.noqapp.android.common.beans.ErrorEncounteredJson;
+import com.noqapp.android.common.beans.JsonResponse;
+import com.noqapp.android.common.beans.JsonSchedule;
+import com.noqapp.android.common.beans.JsonScheduleList;
+import com.noqapp.android.common.presenter.AppointmentPresenter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PastAppointmentFragment extends Fragment implements AppointmentPresenter,
+        MyAppointmentAdapter.OnItemClickListener {
+    private RecyclerView rcv_appointments;
+    private RelativeLayout rl_empty;
+    private List<JsonSchedule> jsonSchedules = new ArrayList<>();
+    protected ProgressDialog progressDialog;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_past_appointment, container, false);
+        rcv_appointments = view.findViewById(R.id.rcv_appointments);
+        rl_empty = view.findViewById(R.id.rl_empty);
+        rcv_appointments.setHasFixedSize(true);
+        rcv_appointments.setLayoutManager(new LinearLayoutManager(getActivity(),
+                RecyclerView.VERTICAL, false));
+        rcv_appointments.setItemAnimator(new DefaultItemAnimator());
+        if (jsonSchedules.size() <= 0) {
+            rcv_appointments.setVisibility(View.GONE);
+            rl_empty.setVisibility(View.VISIBLE);
+        } else {
+            rcv_appointments.setVisibility(View.VISIBLE);
+            rl_empty.setVisibility(View.GONE);
+        }
+        initProgress();
+        if (LaunchActivity.getLaunchActivity().isOnline()) {
+            progressDialog.setMessage("Fetching appointments...");
+            progressDialog.show();
+            AppointmentApiCalls appointmentApiCalls = new AppointmentApiCalls();
+            appointmentApiCalls.setAppointmentPresenter(this);
+            appointmentApiCalls.allPastAppointments(UserUtils.getDeviceId(),
+                    UserUtils.getEmail(),
+                    UserUtils.getAuth());
+        } else {
+            ShowAlertInformation.showNetworkDialog(getActivity());
+        }
+        return view;
+    }
+
+    @Override
+    public void appointmentResponse(JsonScheduleList jsonScheduleList) {
+        Log.e("all appointments", jsonScheduleList.toString());
+        jsonSchedules = jsonScheduleList.getJsonSchedules();
+        if (jsonSchedules.size() <= 0) {
+            rcv_appointments.setVisibility(View.GONE);
+            rl_empty.setVisibility(View.VISIBLE);
+        } else {
+            rcv_appointments.setVisibility(View.VISIBLE);
+            rl_empty.setVisibility(View.GONE);
+        }
+        MyAppointmentAdapter appointmentListAdapter = new MyAppointmentAdapter(
+                jsonSchedules, getActivity(), this);
+        rcv_appointments.setAdapter(appointmentListAdapter);
+        dismissProgress();
+    }
+
+    @Override
+    public void appointmentBookingResponse(JsonSchedule jsonSchedule) {
+        dismissProgress();
+    }
+
+    @Override
+    public void appointmentCancelResponse(JsonResponse jsonResponse) {
+        dismissProgress();
+    }
+
+    @Override
+    public void authenticationFailure() {
+        dismissProgress();
+        AppUtilities.authenticationProcessing(getActivity());
+    }
+
+    @Override
+    public void responseErrorPresenter(ErrorEncounteredJson eej) {
+        dismissProgress();
+        if (null != eej)
+            new ErrorResponseHandler().processError(getActivity(), eej);
+    }
+
+    @Override
+    public void responseErrorPresenter(int errorCode) {
+        dismissProgress();
+        new ErrorResponseHandler().processFailureResponseCode(getActivity(), errorCode);
+    }
+
+
+    @Override
+    public void appointmentDetails(JsonSchedule jsonSchedule) {
+        Intent intent = new Intent(getActivity(), AppointmentBookingDetailActivity.class);
+        intent.putExtra(IBConstant.KEY_DATA_OBJECT, jsonSchedule);
+        intent.putExtra(IBConstant.KEY_FROM_LIST, true);
+        startActivity(intent);
+    }
+
+    private void initProgress() {
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Loading data...");
+    }
+
+    protected void dismissProgress() {
+        if (null != progressDialog && progressDialog.isShowing())
+            progressDialog.dismiss();
+    }
+}
