fc6040f856acf55dc752c865ef711bcbd48de5f1
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2019-06-04 11:05:31 +0530

    Title: Custom Toast added in common library

---
+++ common/src/main/java/com/noqapp/android/common/customviews/CustomToast.java
@@ -0,0 +1,27 @@
+package com.noqapp.android.common.customviews;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.noqapp.android.common.R;
+
+public class CustomToast {
+
+    public void showToast(Context context, String msg) {
+        View layout = LayoutInflater.from(context).inflate(R.layout.custom_toast, null);
+        TextView toastMessage = layout.findViewById(R.id.tv_toast_msg);
+        toastMessage.setText(msg);
+        toastMessage.setTextSize(18);
+        toastMessage.setTextColor(Color.WHITE);
+        toastMessage.setGravity(Gravity.CENTER);
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+}
