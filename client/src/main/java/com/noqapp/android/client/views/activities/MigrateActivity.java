fed2466ee61e8e01c8e6bdb678471fe52315e06a
hth <hth@users.noreply.github.com>
2019-06-05 01:33:28 +0530

    Updated text

--- client/src/main/java/com/noqapp/android/client/views/activities/MigrateActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/MigrateActivity.java
@@ -27,7 +27,7 @@ public class MigrateActivity extends OTPActivity {
         super.onCreate(savedInstanceState);
         activity = this;
         tv_toolbar_title.setText("Migrate Number");
-        btn_login.setText("Start Migrate");
+        btn_login.setText("Start Migration");
     }
 
     @Override
