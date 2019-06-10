d034702a69666a943c916ab491228aede2ef4534
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-06-10 10:19:55 +0530

    Title : App update activity converted to Kotlin class; 1st step to migrate towards kotlin

---
+++ common/src/main/java/com/noqapp/android/common/views/activities/AppUpdateActivity.kt
@@ -0,0 +1,28 @@
+package com.noqapp.android.common.views.activities
+
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.noqapp.android.common.R
+import com.noqapp.android.common.utils.CommonHelper
+
+
+class AppUpdateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.act_app_update)
+        val btn_download = findViewById<Button>(R.id.btn_download)
+        btn_download.setOnClickListener { CommonHelper.openPlayStore(this@AppUpdateActivity) }
+    }
+
+    override fun onBackPressed() {
+
+    }
+}
