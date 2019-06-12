f6af09e51325d873f63f843bcd3abd8699634394
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2019-06-12 16:20:44 +0530

    Title : Client - Tab based screen optimize; Order&Q history ; My Appointments; Coupons Activities

--- client/src/main/java/com/noqapp/android/client/views/activities/MyAppointmentsActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/MyAppointmentsActivity.java
@@ -1,62 +1,27 @@
 package com.noqapp.android.client.views.activities;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.tabs.TabLayout;
 import com.noqapp.android.client.R;
 import com.noqapp.android.client.views.adapters.TabViewPagerAdapter;
-import com.noqapp.android.client.views.fragments.UpcomingAppointmentFragment;
 import com.noqapp.android.client.views.fragments.PastAppointmentFragment;
+import com.noqapp.android.client.views.fragments.UpcomingAppointmentFragment;
 
-public class MyAppointmentsActivity extends BaseActivity {
-
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private LoadTabs loadTabs;
-
+public class MyAppointmentsActivity extends TabbedActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tab);
-        initActionsViews(false);
         tv_toolbar_title.setText(getString(R.string.my_appointments));
-        viewPager = findViewById(R.id.viewpager);
-        tabLayout = findViewById(R.id.tabs);
-        loadTabs = new LoadTabs();
-        loadTabs.execute();
     }
 
-    private void setupViewPager(ViewPager viewPager) {
+    @Override
+    protected void setupViewPager(ViewPager viewPager) {
         TabViewPagerAdapter adapter = new TabViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new UpcomingAppointmentFragment(), "Upcoming");
         adapter.addFragment(new PastAppointmentFragment(), "Past");
         viewPager.setAdapter(adapter);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (null != loadTabs) {
-            loadTabs.cancel(true);
-        }
-    }
-
-    private class LoadTabs extends AsyncTask<String, String, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            return null;
-        }
-
-        protected void onPostExecute(String result) {
-            try {
-                setupViewPager(viewPager);
-                tabLayout.setupWithViewPager(viewPager);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
