c80f595d0957478e6beae95df69e41caa664e7f6
hth <hth@users.noreply.github.com>
2019-06-26 20:16:17 +0530

    Updated text

--- merchant/src/healthcare/java/com/noqapp/android/merchant/views/adapters/AppointmentListAdapter.java
+++ merchant/src/healthcare/java/com/noqapp/android/merchant/views/adapters/AppointmentListAdapter.java
@@ -142,10 +142,10 @@ public class AppointmentListAdapter extends RecyclerView.Adapter<AppointmentList
                             listener.appointmentReject(jsonSchedule, position);
                             break;
                         case R:
-                            new CustomToast().showToast(context, "Appointment already rejected. It cannot be reversed");
+                            new CustomToast().showToast(context, "Appointment already rejected. It cannot be modified");
                             break;
                         case S:
-                            new CustomToast().showToast(context, "Appointment already serviced. It cannot be reversed");
+                            new CustomToast().showToast(context, "Appointment already serviced. It cannot be modified");
                             break;
                     }
                 }
@@ -163,10 +163,10 @@ public class AppointmentListAdapter extends RecyclerView.Adapter<AppointmentList
                             new CustomToast().showToast(context, "Appointment already accepted.");
                             break;
                         case R:
-                            new CustomToast().showToast(context, "Appointment already rejected. It cannot be reversed");
+                            new CustomToast().showToast(context, "Appointment already rejected. It cannot be modified");
                             break;
                         case S:
-                            new CustomToast().showToast(context, "Appointment already serviced. It cannot be reversed");
+                            new CustomToast().showToast(context, "Appointment already serviced. It cannot be modified");
                             break;
                     }
                 }
