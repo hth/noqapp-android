1cae9d10b352ef653b9c4b90222cb13403d1d34f
hth <hth@users.noreply.github.com>
2019-06-27 00:34:22 +0530

    Added method to filter personal coupon based on selected store

--- client/src/main/java/com/noqapp/android/client/model/response/api/ClientCouponApiUrls.java
+++ client/src/main/java/com/noqapp/android/client/model/response/api/ClientCouponApiUrls.java
@@ -10,6 +10,7 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 /**
  * User: hitender
@@ -35,6 +36,29 @@ public interface ClientCouponApiUrls {
 
             @Header("X-R-AUTH")
             String auth
+    );
+
+    /**
+     * Errors
+     * {@link javax.servlet.http.HttpServletResponse#SC_UNAUTHORIZED} - HTTP STATUS 401
+     * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#SEVERE}
+     */
+    @GET("api/c/coupon/filter/{codeQR}.json")
+    Call<JsonCouponList> filterCoupon(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth,
+
+            @Path("codeQR")
+            String codeQR
     );
 
     /**
