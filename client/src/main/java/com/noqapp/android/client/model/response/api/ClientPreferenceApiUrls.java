72d7a6d25243e1bad59a5c0121cdcb67d32a089e
hth <hth@users.noreply.github.com>
2019-06-26 14:23:33 +0530

    Added API to set client preference

---
+++ client/src/main/java/com/noqapp/android/client/model/response/api/ClientPreferenceApiUrls.java
@@ -0,0 +1,51 @@
+package com.noqapp.android.client.model.response.api;
+
+import com.noqapp.android.common.beans.JsonUserPreference;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface ClientPreferenceApiUrls {
+
+    /**
+     * Errors
+     * {@link javax.servlet.http.HttpServletResponse#SC_UNAUTHORIZED} - HTTP STATUS 401
+     * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#SEVERE}
+     */
+    @GET("api/c/preference/notificationSound.json")
+    Call<JsonUserPreference> notificationSound(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth
+    );
+
+    /**
+     * Errors
+     * {@link javax.servlet.http.HttpServletResponse#SC_UNAUTHORIZED} - HTTP STATUS 401
+     * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#SEVERE}
+     */
+    @POST("api/c/preference/promotionalSMS.json")
+    Call<JsonUserPreference> promotionalSMS(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth
+    );
+}
