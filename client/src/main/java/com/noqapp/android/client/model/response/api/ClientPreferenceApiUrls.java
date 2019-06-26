9d88901f731a59d0aa4b11c0d592a871a0fdaf97
hth <hth@users.noreply.github.com>
2019-06-26 20:17:28 +0530

    Optimized import

--- client/src/main/java/com/noqapp/android/client/model/response/api/ClientPreferenceApiUrls.java
+++ client/src/main/java/com/noqapp/android/client/model/response/api/ClientPreferenceApiUrls.java
@@ -3,7 +3,6 @@ package com.noqapp.android.client.model.response.api;
 import com.noqapp.android.common.beans.JsonUserPreference;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
