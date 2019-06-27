3d18c55b41284806dbd86a0c44c5d6a1fb50deb7
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2019-06-27 12:12:23 +0530

    Title : CustomProgress add to common so that i can used by other clases with activity & fragment

--- merchant/src/main/java/com/noqapp/android/merchant/views/activities/BaseActivity.java
+++ merchant/src/main/java/com/noqapp/android/merchant/views/activities/BaseActivity.java
@@ -1,65 +1,45 @@
 package com.noqapp.android.merchant.views.activities;
 
-import android.app.Dialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.noqapp.android.common.beans.ErrorEncounteredJson;
 import com.noqapp.android.common.presenter.ResponseErrorPresenter;
-import com.noqapp.android.merchant.R;
+import com.noqapp.android.common.utils.CustomProgressBar;
 import com.noqapp.android.merchant.utils.AppUtils;
 import com.noqapp.android.merchant.utils.ErrorResponseHandler;
 
 public class BaseActivity extends AppCompatActivity implements ResponseErrorPresenter {
 
-    private Dialog dialog;
-    private TextView tv_loading_msg;
     protected ImageView iv_home;
     protected ImageView actionbarBack;
     protected TextView tv_toolbar_title;
+    private CustomProgressBar customProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initProgress();
-    }
-
-    private void initProgress() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View view = inflater.inflate(R.layout.lay_progress, null, false);
-        tv_loading_msg = view.findViewById(R.id.tv_loading_msg);
-        builder.setView(view);
-        dialog = builder.create();
-    }
-
-    protected void setProgressCancel(boolean isCancelled) {
-        if (null != dialog && dialog.isShowing()) {
-            dialog.setCanceledOnTouchOutside(isCancelled);
-            dialog.setCancelable(isCancelled);
-        }
+        customProgressBar = new CustomProgressBar(this);
     }
 
     protected void dismissProgress() {
-        if (null != dialog && dialog.isShowing())
-            dialog.dismiss();
+        customProgressBar.dismissProgress();
     }
 
     protected void showProgress() {
-        if (null != dialog)
-            dialog.show();
+        customProgressBar.showProgress();
+    }
+
+    protected void setProgressCancel(boolean isCancelled) {
+        customProgressBar.setProgressCancel(isCancelled);
     }
 
     protected void setProgressMessage(String msg) {
-        tv_loading_msg.setText(msg);
+        customProgressBar.setProgressMessage(msg);
     }
-    
 
     @Override
     public void responseErrorPresenter(ErrorEncounteredJson eej) {
--- merchant/src/main/java/com/noqapp/android/merchant/views/fragments/BaseFragment.java
+++ merchant/src/main/java/com/noqapp/android/merchant/views/fragments/BaseFragment.java
@@ -1,61 +1,43 @@
 package com.noqapp.android.merchant.views.fragments;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.noqapp.android.common.beans.ErrorEncounteredJson;
 import com.noqapp.android.common.presenter.ResponseErrorPresenter;
-import com.noqapp.android.merchant.R;
+import com.noqapp.android.common.utils.CustomProgressBar;
 import com.noqapp.android.merchant.utils.AppUtils;
 import com.noqapp.android.merchant.utils.ErrorResponseHandler;
 
 public class BaseFragment extends Fragment implements ResponseErrorPresenter {
-    private Dialog dialog;
-    private TextView tv_loading_msg;
+    private CustomProgressBar customProgressBar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        initProgress();
+        customProgressBar = new CustomProgressBar(getActivity());
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
-    private void initProgress() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = LayoutInflater.from(getActivity());
-        View view = inflater.inflate(R.layout.lay_progress, null, false);
-        tv_loading_msg = view.findViewById(R.id.tv_loading_msg);
-        builder.setView(view);
-        dialog = builder.create();
-    }
-
     protected void dismissProgress() {
-        if (null != dialog && dialog.isShowing())
-            dialog.dismiss();
+        customProgressBar.dismissProgress();
     }
 
     protected void showProgress() {
-        if (null != dialog)
-            dialog.show();
+        customProgressBar.showProgress();
     }
 
     protected void setProgressCancel(boolean isCancelled) {
-        if (null != dialog && dialog.isShowing()) {
-            dialog.setCanceledOnTouchOutside(isCancelled);
-            dialog.setCancelable(isCancelled);
-        }
+        customProgressBar.setProgressCancel(isCancelled);
     }
 
     protected void setProgressMessage(String msg) {
-        tv_loading_msg.setText(msg);
+       customProgressBar.setProgressMessage(msg);
     }
 
     @Override
