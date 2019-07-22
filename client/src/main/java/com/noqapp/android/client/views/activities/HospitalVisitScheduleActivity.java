995570c37489e29d868d16498d0c9a003d0af896
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2019-07-22 15:40:38 +0530

    Title : Client - HospitalVisitScchedule screen convert to tabbased

--- client/src/main/java/com/noqapp/android/client/views/activities/HospitalVisitScheduleActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/HospitalVisitScheduleActivity.java
@@ -3,38 +3,32 @@ package com.noqapp.android.client.views.activities;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
-import com.noqapp.android.client.R;
 import com.noqapp.android.client.model.UserMedicalProfileApiCalls;
 import com.noqapp.android.client.presenter.beans.body.MedicalProfile;
 import com.noqapp.android.client.utils.NetworkUtils;
 import com.noqapp.android.client.utils.ShowAlertInformation;
 import com.noqapp.android.client.utils.UserUtils;
-import com.noqapp.android.client.views.adapters.HospitalVisitScheduleAdapter;
+import com.noqapp.android.client.views.adapters.TabViewPagerAdapter;
+import com.noqapp.android.client.views.fragments.HospitalVisitScheduleFragment;
 import com.noqapp.android.common.beans.medical.JsonHospitalVisitSchedule;
 import com.noqapp.android.common.beans.medical.JsonHospitalVisitScheduleList;
 import com.noqapp.android.common.customviews.CustomToast;
+import com.noqapp.android.common.model.types.medical.HospitalVisitForEnum;
 import com.noqapp.android.common.presenter.HospitalVisitSchedulePresenter;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class HospitalVisitScheduleActivity extends BaseActivity implements
+public class HospitalVisitScheduleActivity extends TabbedActivity implements
         HospitalVisitSchedulePresenter {
-    private RecyclerView rcv_header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_immunization);
-        initActionsViews(true);
         tv_toolbar_title.setText("Hospital Visit Schedule");
-        rcv_header = findViewById(R.id.rcv_header);
-        rcv_header.setHasFixedSize(true);
-        rcv_header.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        rcv_header.setItemAnimator(new DefaultItemAnimator());
         MedicalProfile medicalProfile = (MedicalProfile) getIntent().getSerializableExtra("medicalProfile");
         UserMedicalProfileApiCalls userMedicalProfileApiCalls = new UserMedicalProfileApiCalls();
         userMedicalProfileApiCalls.setHospitalVisitSchedulePresenter(this);
@@ -52,12 +46,40 @@ public class HospitalVisitScheduleActivity extends BaseActivity implements
 
     }
 
+
+    @Override
+    protected void setupViewPager(ViewPager viewPager) {
+
+    }
+
     @Override
     public void hospitalVisitScheduleResponse(JsonHospitalVisitScheduleList jsonHospitalVisitScheduleList) {
         Log.e("immunization", jsonHospitalVisitScheduleList.toString());
         List<JsonHospitalVisitSchedule> jsonHospitalVisitSchedules = jsonHospitalVisitScheduleList.getJsonHospitalVisitSchedules();
-        HospitalVisitScheduleAdapter hospitalVisitScheduleAdapter = new HospitalVisitScheduleAdapter(this, jsonHospitalVisitSchedules,
-                null);
-        rcv_header.setAdapter(hospitalVisitScheduleAdapter);
+
+        List<JsonHospitalVisitSchedule> immunizationList = new ArrayList<>();
+        List<JsonHospitalVisitSchedule> vaccinationList = new ArrayList<>();
+        for (int i = 0; i < jsonHospitalVisitSchedules.size(); i++) {
+            if (jsonHospitalVisitSchedules.get(i).getHospitalVisitFor() == HospitalVisitForEnum.IMU) {
+                immunizationList.add(jsonHospitalVisitSchedules.get(i));
+            } else {
+                vaccinationList.add(jsonHospitalVisitSchedules.get(i));
+            }
+        }
+        HospitalVisitScheduleFragment hvsfVaccine = new HospitalVisitScheduleFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("data", (Serializable) vaccinationList);
+        hvsfVaccine.setArguments(bundle);
+
+        HospitalVisitScheduleFragment hvsfImmune = new HospitalVisitScheduleFragment();
+        Bundle b = new Bundle();
+        b.putSerializable("data", (Serializable) immunizationList);
+        hvsfImmune.setArguments(b);
+        TabViewPagerAdapter adapter = new TabViewPagerAdapter(getSupportFragmentManager());
+        if (vaccinationList.size() > 0)
+            adapter.addFragment(hvsfVaccine, "Vaccination");
+        if (immunizationList.size() > 0)
+            adapter.addFragment(hvsfImmune, "Immunization");
+        viewPager.setAdapter(adapter);
     }
 }
