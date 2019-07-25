7ecd7c7e1c4c5ac16e80dec343f78417c2d90c9b
hth <hth@users.noreply.github.com>
2019-07-25 22:09:26 +0530

    Clean up

--- client/src/main/java/com/noqapp/android/client/presenter/TokenPresenter.java
+++ client/src/main/java/com/noqapp/android/client/presenter/TokenPresenter.java
@@ -7,7 +7,6 @@ import com.noqapp.android.common.presenter.ResponseErrorPresenter;
  * User: hitender
  * Date: 4/1/17 12:22 PM
  */
-
 public interface TokenPresenter extends ResponseErrorPresenter {
 
     void tokenPresenterResponse(JsonToken token);
