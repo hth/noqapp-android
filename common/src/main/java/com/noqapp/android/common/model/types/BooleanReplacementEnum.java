a350e66978191ac0db8a4679e5bf72f84f59eb49
hth <hth@users.noreply.github.com>
2019-07-25 07:43:06 +0530

    Changed color

--- common/src/main/java/com/noqapp/android/common/model/types/BooleanReplacementEnum.java
+++ common/src/main/java/com/noqapp/android/common/model/types/BooleanReplacementEnum.java
@@ -8,9 +8,9 @@ import java.util.List;
  * Date: 2019-07-22 11:07
  */
 public enum BooleanReplacementEnum {
-    S("S", "Skip", "#f56942"),
-    Y("Y", "Yes", "#362926"),
-    N("N", "No", "#63443c");
+    S("S", "Skip", "#CD334E"),
+    Y("Y", "Yes", "#19769f"),
+    N("N", "No", "#ddd");
 
     private final String description;
     private final String name;
