cc7e18428f14ab0d774672242eb267549f0165df
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-09-05 11:04:04 +0530

    Merchant-health  ---Empty workdone view added

--- merchant/src/healthcare/java/com/noqapp/android/merchant/views/activities/PatientProfileActivity.java
+++ merchant/src/healthcare/java/com/noqapp/android/merchant/views/activities/PatientProfileActivity.java
@@ -65,6 +65,7 @@ public class PatientProfileActivity extends BaseActivity implements
     private boolean isDental = false;
     private String bizCategoryId = "";
     private ListView list_view;
+    private TextView tv_empty_work_done;
     private ArrayList<ToothWorkDone> toothWorkDoneList = new ArrayList<>();
     private WorkDoneAdapter workDoneAdapter;
 
@@ -92,6 +93,7 @@ public class PatientProfileActivity extends BaseActivity implements
 
         ll_dental_history = findViewById(R.id.ll_dental_history);
         list_view = findViewById(R.id.list_view);
+        tv_empty_work_done = findViewById(R.id.tv_empty_work_done);
         tv_patient_name = findViewById(R.id.tv_patient_name);
         tv_address = findViewById(R.id.tv_address);
         tv_details = findViewById(R.id.tv_details);
@@ -386,6 +388,7 @@ public class PatientProfileActivity extends BaseActivity implements
         }
         workDoneAdapter = new WorkDoneAdapter(this, toothWorkDoneList);
         list_view.setAdapter(workDoneAdapter);
+        tv_empty_work_done.setVisibility(toothWorkDoneList.size()>0 ? View.GONE:View.VISIBLE);
     }
 
     public void parseWorkDoneData(String str, String createdDate) {
