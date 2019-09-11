3d620de272ba1e8a141ef0d3d3173b8b7cba1f97
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-09-11 11:17:33 +0530

    Merchant-Dental > image upload code removed from dental anatomy

--- merchant/src/healthcare/java/com/noqapp/android/merchant/views/fragments/DentalProDiagnosisFragment.java
+++ merchant/src/healthcare/java/com/noqapp/android/merchant/views/fragments/DentalProDiagnosisFragment.java
@@ -1,42 +1,28 @@
 package com.noqapp.android.merchant.views.fragments;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ScrollView;
 
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.noqapp.android.common.customviews.CustomToast;
 import com.noqapp.android.common.model.types.medical.DentalOptionEnum;
 import com.noqapp.android.merchant.R;
-import com.noqapp.android.merchant.model.database.utils.MedicalFilesDB;
 import com.noqapp.android.merchant.views.activities.MedicalCaseActivity;
 import com.noqapp.android.merchant.views.adapters.ToothAdapter;
 import com.noqapp.android.merchant.views.pojos.ToothInfo;
 import com.noqapp.android.merchant.views.pojos.ToothProcedure;
 import com.noqapp.android.merchant.views.utils.MedicalDataStatic;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class DentalProDiagnosisFragment extends BaseFragment {
-    private View view;
     private ToothAdapter toothAdapter;
     private RecyclerView rcv_tooth;
 
@@ -45,15 +31,10 @@ public class DentalProDiagnosisFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        view = inflater.inflate(R.layout.frag_dental, container, false);
+        View view = inflater.inflate(R.layout.frag_dental, container, false);
         rcv_tooth = view.findViewById(R.id.rcv_tooth);
         rcv_tooth.setLayoutManager(new GridLayoutManager(getActivity(), 16));
         rcv_tooth.setItemAnimator(new DefaultItemAnimator());
-        Button btn_save_upload = view.findViewById(R.id.btn_save_upload);
-        btn_save_upload.setVisibility(View.GONE);
-        btn_save_upload.setOnClickListener(v -> {
-            getCaptureAndUploadBitmap();
-        });
         return view;
     }
 
@@ -69,7 +50,7 @@ public class DentalProDiagnosisFragment extends BaseFragment {
             ToothInfo toothInfo = new ToothInfo();
             toothInfo.setToothNumber(Integer.parseInt(toothNumbers.get(i)));
             //toothInfo.setToothFrontView(drawables.get(i));
-            toothInfo.setToothFrontView(getTempDrawable(i+1));
+            toothInfo.setToothFrontView(getTempDrawable(i + 1));
             toothInfo.setToothDefaultFrontView(toothInfo.getToothFrontView());
             toothInfo.setToothTopView(imageFilePathTop);
             toothInfo.setToothDefaultTopView(toothInfo.getToothTopView());
@@ -84,59 +65,6 @@ public class DentalProDiagnosisFragment extends BaseFragment {
                     MedicalCaseActivity.getMedicalCaseActivity().getJsonMedicalRecord().getJsonUserMedicalProfile().getDentalAnatomy())) {
                 String temp = MedicalCaseActivity.getMedicalCaseActivity().getJsonMedicalRecord().getJsonUserMedicalProfile().getDentalAnatomy();
                 toothAdapter.updateToothObj(temp);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void getCaptureAndUploadBitmap() {
-        try {
-            View u = view.findViewById(R.id.scroll);
-            u.setDrawingCacheEnabled(true);
-            ScrollView z = view.findViewById(R.id.scroll);
-            z.setBackgroundColor(getResources().getColor(R.color.white));
-            int x = z.getChildAt(0).getTop();
-            int y = z.getChildAt(0).getLeft();
-            int totalHeight = z.getChildAt(0).getHeight();
-            int totalWidth = z.getChildAt(0).getWidth();
-            u.layout(0, 0, totalWidth, totalHeight);
-            u.buildDrawingCache(true);
-            Bitmap bitmap = Bitmap.createBitmap(u.getDrawingCache());
-            u.setDrawingCacheEnabled(false);
-
-            //Save bitmap
-            File folder = new File(Environment.getExternalStorageDirectory() + File.separator + "NoQueue");
-            if (!folder.exists()) {
-                folder.mkdirs();
-            }
-            String extr = Environment.getExternalStorageDirectory().toString() + File.separator + "NoQueue";
-            //  String fileName = new SimpleDateFormat("yyyyMMddhhmm'_report.jpg'").format(new Date());
-            String fileName = new SimpleDateFormat("'NoQueue_" + MedicalCaseActivity.getMedicalCaseActivity().jsonQueuedPerson.getCustomerName() + "_'yyyyMMddhhmm'.jpg'", Locale.getDefault()).format(new Date());
-
-            File myPath = new File(extr, fileName);
-            FileOutputStream fos = null;
-            try {
-                fos = new FileOutputStream(myPath);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-                fos.close();
-                MediaStore.Images.Media.insertImage(getActivity().getContentResolver(), bitmap, "Screen", "screen");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (null != fos)
-                    fos.flush();
-                if (null != bitmap) {
-                    bitmap.recycle();
-                    bitmap = null;
-                }
-            }
-            u.layout(x, y, totalWidth, totalHeight);
-            if (myPath.exists()) {
-                MedicalFilesDB.insertMedicalFile(MedicalCaseActivity.getMedicalCaseActivity().jsonMedicalRecord.getRecordReferenceId(), myPath.getAbsolutePath());
-                new CustomToast().showToast(getActivity(), "Saved image. This image will be available in case history.");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -193,19 +121,19 @@ public class DentalProDiagnosisFragment extends BaseFragment {
 
 
     private ToothProcedure getTempDrawable(int pos) {
-        if (pos == 1 ||pos == 2 ||pos == 3 ||pos == 16 ||pos == 15 ||pos == 14 ) {
+        if (pos == 1 || pos == 2 || pos == 3 || pos == 16 || pos == 15 || pos == 14) {
             return new ToothProcedure(R.drawable.t_1, DentalOptionEnum.NOR.getDescription());
         } else if (pos == 4 || pos == 13) {
             return new ToothProcedure(R.drawable.t_2, DentalOptionEnum.NOR.getDescription());
-        } else if (pos > 4 && pos <13) {
+        } else if (pos > 4 && pos < 13) {
             return new ToothProcedure(R.drawable.t_3, DentalOptionEnum.NOR.getDescription());
-        } else  if (pos == 17 ||pos == 18 ||pos == 19 ||pos == 32 ||pos == 31 ||pos == 30 ) {
+        } else if (pos == 17 || pos == 18 || pos == 19 || pos == 32 || pos == 31 || pos == 30) {
             return new ToothProcedure(R.drawable.t_4, DentalOptionEnum.NOR.getDescription());
         } else if (pos == 20 || pos == 29) {
             return new ToothProcedure(R.drawable.t_5, DentalOptionEnum.NOR.getDescription());
-        } else if (pos > 20 && pos <29) {
+        } else if (pos > 20 && pos < 29) {
             return new ToothProcedure(R.drawable.t_6, DentalOptionEnum.NOR.getDescription());
-        }else{
+        } else {
             return new ToothProcedure(R.drawable.t_3, DentalOptionEnum.NOR.getDescription());
         }
     }
