b39c9bed496076d33ccedd14358da27915be9899
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2019-10-09 18:54:09 +0530

    Title : Address book screen UI updated

--- client/src/main/java/com/noqapp/android/client/views/activities/AddressBookActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/AddressBookActivity.java
@@ -5,23 +5,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.AppCompatRadioButton;
 
 import com.noqapp.android.client.R;
 import com.noqapp.android.client.model.ClientProfileApiCall;
 import com.noqapp.android.client.presenter.ProfileAddressPresenter;
+import com.noqapp.android.client.utils.AppUtils;
 import com.noqapp.android.client.utils.ShowAlertInformation;
 import com.noqapp.android.client.utils.UserUtils;
 import com.noqapp.android.client.views.adapters.AddressListAdapter;
@@ -33,9 +31,8 @@ import java.util.List;
 
 
 public class AddressBookActivity extends BaseActivity implements ProfileAddressPresenter, AddressListAdapter.RemoveAddress {
-    private RadioGroup rg_address;
     private EditText edt_add_address;
-    private Button btn_add_address;
+    private LinearLayout ll_add_address;
     private ClientProfileApiCall clientProfileApiCall;
     private ListView lv_address;
 
@@ -50,14 +47,14 @@ public class AddressBookActivity extends BaseActivity implements ProfileAddressP
         });
         lv_address = findViewById(R.id.listview_address);
         tv_toolbar_title.setText(getString(R.string.screen_addressbook));
-        rg_address = findViewById(R.id.rg_address);
         edt_add_address = findViewById(R.id.edt_add_address);
-        btn_add_address = findViewById(R.id.btn_add_address);
+        Button btn_add_address = findViewById(R.id.btn_add_address);
         btn_add_address.setOnClickListener((View v) -> {
             edt_add_address.setError(null);
             if (TextUtils.isEmpty(edt_add_address.getText().toString())) {
                 edt_add_address.setError(getString(R.string.error_field_required));
             } else {
+                AppUtils.hideKeyBoard(AddressBookActivity.this);
                 if (LaunchActivity.getLaunchActivity().isOnline()) {
                     showProgress();
                     setProgressMessage("Adding address in progress..");
@@ -66,11 +63,17 @@ public class AddressBookActivity extends BaseActivity implements ProfileAddressP
                 }
             }
         });
+        ll_add_address = findViewById(R.id.ll_add_address);
         TextView tv_add_address = findViewById(R.id.tv_add_address);
         tv_add_address.setOnClickListener((View v) -> {
-            edt_add_address.setVisibility(View.VISIBLE);
-            btn_add_address.setVisibility(View.VISIBLE);
+            ll_add_address.setVisibility(View.VISIBLE);
         });
+        ImageView iv_close = findViewById(R.id.iv_close);
+        iv_close.setOnClickListener((View v) -> {
+            ll_add_address.setVisibility(View.GONE);
+        });
+
+
         clientProfileApiCall = new ClientProfileApiCall();
         clientProfileApiCall.setProfileAddressPresenter(this);
         JsonUserAddressList jsonUserAddressList = new JsonUserAddressList();
@@ -100,84 +103,9 @@ public class AddressBookActivity extends BaseActivity implements ProfileAddressP
                 finish();
             }
         });
-        rg_address.removeAllViews();
-        for (int i = 0; i < addressList.size(); i++) {
-
-            LayoutInflater inflater = LayoutInflater.from(this);
-            View radio_view = inflater.inflate(R.layout.list_item_radio, null, false);
-            final AppCompatRadioButton rdbtn = radio_view.findViewById(R.id.acrb);
-            rdbtn.setId(i);
-            rdbtn.setTag(addressList.get(i).getId());
-            rdbtn.setText(addressList.get(i).getAddress());
-            rdbtn.setPadding(10, 20, 10, 20);
-            rdbtn.setLayoutParams(
-                    new RadioGroup.LayoutParams(
-                            android.view.ViewGroup.LayoutParams.FILL_PARENT,
-                            android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-                    )
-            );
-            rdbtn.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    final int DRAWABLE_RIGHT = 2;
-                    if (event.getAction() == MotionEvent.ACTION_UP) {
-                        if (event.getRawX() >= (rdbtn.getRight() - rdbtn.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-                            // your action here
-                            AlertDialog.Builder builder = new AlertDialog.Builder(AddressBookActivity.this);
-                            LayoutInflater inflater = LayoutInflater.from(AddressBookActivity.this);
-                            builder.setTitle(null);
-                            View customDialogView = inflater.inflate(R.layout.dialog_general, null, false);
-                            builder.setView(customDialogView);
-                            final AlertDialog mAlertDialog = builder.create();
-                            mAlertDialog.setCanceledOnTouchOutside(false);
-                            TextView tvtitle = customDialogView.findViewById(R.id.tvtitle);
-                            TextView tv_msg = customDialogView.findViewById(R.id.tv_msg);
-                            tvtitle.setText("Delete Address");
-                            tv_msg.setText("Do you want to delete address from address list?");
-                            Button btn_yes = customDialogView.findViewById(R.id.btn_yes);
-                            Button btn_no = customDialogView.findViewById(R.id.btn_no);
-                            View separator = customDialogView.findViewById(R.id.seperator);
-                            btn_no.setVisibility(View.VISIBLE);
-                            separator.setVisibility(View.VISIBLE);
-                            btn_yes.setOnClickListener((View view) -> {
-                                if (LaunchActivity.getLaunchActivity().isOnline()) {
-                                    showProgress();
-                                    setProgressMessage("Deleting address..");
-                                    clientProfileApiCall.deleteProfileAddress(UserUtils.getEmail(), UserUtils.getAuth(), new JsonUserAddress().setAddress(rdbtn.getText().toString()).setId(rdbtn.getTag().toString()));
-                                } else {
-                                    ShowAlertInformation.showNetworkDialog(AddressBookActivity.this);
-                                }
-                                mAlertDialog.dismiss();
-
-                            });
-                            btn_no.setOnClickListener((View view) -> {
-                                mAlertDialog.dismiss();
-                            });
-                            mAlertDialog.show();
-                            return true;
-                        }
-                    }
-                    return false;
-                }
-            });
-            rg_address.addView(rdbtn);
-        }
-        rg_address.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                AppCompatRadioButton radioButtonChosen = group.findViewById(checkedId);
-                JsonUserAddress jsonUserAddress = addressList.get(radioButtonChosen.getId());
-                Intent resultIntent = new Intent();
-                resultIntent.putExtra("jsonUserAddress", jsonUserAddress);
-                setResult(78, resultIntent);
-                //setResult(Activity.RESULT_OK, resultIntent);
-                finish();
-
-            }
-        });
 
         dismissProgress();
-
+        ll_add_address.setVisibility(View.GONE);
     }
 
     @Override
