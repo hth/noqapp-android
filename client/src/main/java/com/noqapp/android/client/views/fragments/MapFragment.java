8f59e0164a986595e76f9730ff326bfbe62a74cd
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2019-11-03 17:13:19 +0530

    Title : Client - map height increase to 400dp; map control hide issue fixed; map code cleanup; manual url builder removed

--- client/src/main/java/com/noqapp/android/client/views/fragments/MapFragment.java
+++ client/src/main/java/com/noqapp/android/client/views/fragments/MapFragment.java
@@ -1,9 +1,15 @@
 package com.noqapp.android.client.views.fragments;
 
-import com.noqapp.android.client.R;
-import com.noqapp.android.client.utils.DirectionsJsonParser;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdate;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,42 +17,24 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.android.PolyUtil;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.TravelMode;
+import com.noqapp.android.client.R;
 
-import org.json.JSONObject;
+import org.joda.time.DateTime;
 
-import android.graphics.Color;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
-    private final String TAG = MapFragment.class.getSimpleName();
-
-    private GoogleMap mMap;
-    private LatLng mOrigin;
-    private LatLng mDestination;
-    private Polyline mPolyline;
-    private ArrayList<LatLng> mMarkerPoints;
+    private LatLng source;
+    private LatLng destination;
+    private static final int overview = 0;
 
     public static MapFragment getInstance(LatLng source, LatLng destination) {
         MapFragment mapFragment = new MapFragment();
@@ -64,157 +52,78 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         View itemView = inflater.inflate(R.layout.frag_map, container, false);
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        mMarkerPoints = new ArrayList<>();
         return itemView;
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-//        LatLng source = new LatLng(19.0237, 73.0404);
-//        LatLng destination = new LatLng(19.1045, 73.0033);
-        LatLng source = (LatLng) getArguments().get("source");
-        LatLng destination = (LatLng) getArguments().get("destination");
-        addAndProcessLocation(source);
-        addAndProcessLocation(destination);
-        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(source, 10);
-        mMap.animateCamera(yourLocation);
+        GoogleMap mMap = googleMap;
+        source = (LatLng) getArguments().get("source");
+        destination = (LatLng) getArguments().get("destination");
+
+        // CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(source, 10);
+        //mMap.animateCamera(yourLocation);
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+
+        DirectionsResult results = getDirectionsDetails();
+        if (results != null) {
+            addPolyline(results, googleMap);
+            positionCamera(results.routes[overview], googleMap);
+            addMarkersToMap(results, googleMap);
+        }
     }
 
-    private void addAndProcessLocation(LatLng point) {
-        if (mMarkerPoints.size() > 2) {
-            mMarkerPoints.clear();
-            mMap.clear();
-        }
-        mMarkerPoints.add(point);
-        MarkerOptions options = new MarkerOptions();
-        options.position(point);
+    private void positionCamera(DirectionsRoute route, GoogleMap mMap) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(route.legs[overview].startLocation.lat, route.legs[overview].startLocation.lng), 12));
+    }
+
+    private void addMarkersToMap(DirectionsResult results, GoogleMap mMap) {
         /* For the start location, the color of marker is GREEN and for the end location, the color of marker is RED. */
-        if (mMarkerPoints.size() == 1) {
-            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-        } else if (mMarkerPoints.size() == 2) {
-            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-        }
-        mMap.addMarker(options);
-        if (mMarkerPoints.size() >= 2) {
-            mOrigin = mMarkerPoints.get(0);
-            mDestination = mMarkerPoints.get(1);
-            drawRoute();
-        }
+        mMap.addMarker(new MarkerOptions().
+                position(new LatLng(results.routes[overview].legs[overview].startLocation.lat, results.routes[overview].legs[overview].startLocation.lng)).title(results.routes[overview].legs[overview].startAddress)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mMap.addMarker(new MarkerOptions().
+                position(new LatLng(results.routes[overview].legs[overview].endLocation.lat, results.routes[overview].legs[overview].endLocation.lng)).title(results.routes[overview].legs[overview].startAddress)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)).snippet(getEndLocationTitle(results)));
     }
 
-    private void drawRoute() {
-        String url = getDirectionsUrl(mOrigin, mDestination);
-        DownloadTask downloadTask = new DownloadTask();
-        downloadTask.execute(url);
-    }
-
-    private String getDirectionsUrl(LatLng origin, LatLng dest) {
-        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-        String key = "key=" + getString(R.string.google_maps_key);
-        String parameters = str_origin + "&" + str_dest + "&" + key;
-        String output = "json";
-        return "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
-    }
-
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
+    private DirectionsResult getDirectionsDetails() {
+        DateTime now = new DateTime();
         try {
-            URL url = new URL(strUrl);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.connect();
-            iStream = urlConnection.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            data = sb.toString();
-            br.close();
+            return DirectionsApi.newRequest(getGeoContext())
+                    .mode(TravelMode.DRIVING)
+                    .origin(new com.google.maps.model.LatLng(source.latitude, source.longitude))
+                    .destination(new com.google.maps.model.LatLng(destination.latitude, destination.longitude))
+                    .departureTime(now)
+                    .await();
         } catch (Exception e) {
-            Log.d("Exception on download", e.toString());
-        } finally {
-            Objects.requireNonNull(iStream).close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
-
-
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... url) {
-            String data = "";
-            try {
-                data = downloadUrl(url[0]);
-                Log.d("DownloadTask", "DownloadTask : " + data);
-            } catch (Exception e) {
-                Log.d("Background Task", e.toString());
-            }
-            return data;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            ParserTask parserTask = new ParserTask();
-            parserTask.execute(result);
+            e.printStackTrace();
+            return null;
         }
     }
 
+    private GeoApiContext getGeoContext() {
+        GeoApiContext geoApiContext = new GeoApiContext();
+        return geoApiContext.setQueryRateLimit(3)
+                .setApiKey(getString(R.string.google_maps_key))
+                .setConnectTimeout(1, TimeUnit.SECONDS)
+                .setReadTimeout(1, TimeUnit.SECONDS)
+                .setWriteTimeout(1, TimeUnit.SECONDS);
+    }
 
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-            List<List<HashMap<String, String>>> routes = null;
-            try {
-                JSONObject jObject = new JSONObject(jsonData[0]);
-                DirectionsJsonParser parser = new DirectionsJsonParser();
-                routes = parser.parse(jObject);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return routes;
-        }
+    private void addPolyline(DirectionsResult results, GoogleMap mMap) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.color(Color.RED);
+        polylineOptions.addAll(decodedPath);
+        mMap.addPolyline(polylineOptions);
 
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            PolylineOptions lineOptions = null;
-            for (int i = 0; i < result.size(); i++) {
-                List<LatLng> points = new ArrayList<>();
-                lineOptions = new PolylineOptions();
-                List<HashMap<String, String>> path = result.get(i);
-                for (int j = 0; j < path.size(); j++) {
-                    try {
-                        HashMap<String, String> point = path.get(j);
-                        double lat = Double.parseDouble(Objects.requireNonNull(point.get("lat")));
-                        double lng = Double.parseDouble(Objects.requireNonNull(point.get("lng")));
-                        LatLng position = new LatLng(lat, lng);
-                        points.add(position);
-                    } catch (NullPointerException e) {
-                        Log.e(TAG, "Found null instead of lat lng", e);
-                    }
-                }
+    }
 
-                lineOptions.addAll(points);
-                lineOptions.width(8);
-                lineOptions.color(Color.RED);
-            }
-
-            if (lineOptions != null) {
-                if (mPolyline != null) {
-                    mPolyline.remove();
-                }
-                mPolyline = mMap.addPolyline(lineOptions);
-            } else {
-                Toast.makeText(getActivity(), "No route information found", Toast.LENGTH_LONG).show();
-            }
-        }
+    private String getEndLocationTitle(DirectionsResult results) {
+        return "Time :" + results.routes[overview].legs[overview].duration.humanReadable + " Distance :" + results.routes[overview].legs[overview].distance.humanReadable;
     }
 }
