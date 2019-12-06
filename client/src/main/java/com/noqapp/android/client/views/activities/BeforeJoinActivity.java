faf03bcdb7b372ce922a33aed120be155d828eab
hth <hth@users.noreply.github.com>
2019-12-07 05:00:27 +0530

    Formatting

--- client/src/main/java/com/noqapp/android/client/views/activities/BeforeJoinActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/BeforeJoinActivity.java
@@ -1,21 +1,5 @@
 package com.noqapp.android.client.views.activities;
 
-import android.content.Intent;
-import android.graphics.Paint;
-import android.os.Bundle;
-import android.text.Html;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import androidx.core.content.ContextCompat;
-
-import com.crashlytics.android.answers.Answers;
-import com.crashlytics.android.answers.CustomEvent;
 import com.noqapp.android.client.R;
 import com.noqapp.android.client.model.QueueApiAuthenticCall;
 import com.noqapp.android.client.model.QueueApiUnAuthenticCall;
@@ -33,9 +17,24 @@ import com.noqapp.android.client.utils.UserUtils;
 import com.noqapp.android.client.views.adapters.DependentAdapter;
 import com.noqapp.android.common.beans.JsonProfile;
 import com.noqapp.android.common.customviews.CustomToast;
-import com.noqapp.android.common.model.types.BusinessTypeEnum;
 import com.noqapp.android.common.utils.PhoneFormatterUtil;
+
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.CustomEvent;
 import com.squareup.picasso.Picasso;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import androidx.core.content.ContextCompat;
 
 import java.util.List;
 
@@ -278,11 +277,11 @@ public class BeforeJoinActivity extends BaseActivity implements QueuePresenter {
                     btn_joinQueue.setText(getString(R.string.login_to_join));
                     btn_pay_and_joinQueue.setText(getString(R.string.login_to_join));
 
-                }else{
+                } else {
                     Intent loginIntent = new Intent(BeforeJoinActivity.this, LoginActivity.class);
                     startActivity(loginIntent);
                 }
-            }else{
+            } else {
                 btn_joinQueue.setText(getString(R.string.join));
                 btn_pay_and_joinQueue.setText(getString(R.string.pay_and_join));
             }
