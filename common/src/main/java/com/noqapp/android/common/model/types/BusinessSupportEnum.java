d050fa1d8bd995aa4e7136d01f12813713267d0f
hth <hth@users.noreply.github.com>
2020-04-17 19:14:13 -0700

    Added missing file

---
+++ common/src/main/java/com/noqapp/android/common/model/types/BusinessSupportEnum.java
@@ -0,0 +1,32 @@
+package com.noqapp.android.common.model.types;
+
+/**
+ * hitender
+ * 4/17/20 9:59 AM
+ */
+public enum BusinessSupportEnum {
+    OD("OD", "Order"),
+    OQ("OQ", "Order but Queue"),
+    QQ("QQ", "Queue");
+
+    private String name;
+    private String description;
+
+    BusinessSupportEnum(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
