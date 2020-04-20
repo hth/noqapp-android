e3cfa1ba0bce16c7d8a360fbd40df792e5c6ae73
hth <hth@users.noreply.github.com>
2020-04-20 00:16:48 -0700

    Updated debug version

--- client/src/main/java/com/noqapp/android/client/utils/Constants.java
+++ client/src/main/java/com/noqapp/android/client/utils/Constants.java
@@ -56,7 +56,7 @@ public class Constants extends BaseConstants{
         if (StringUtils.isBlank(VERSION_RELEASE)) {
             switch (BuildConfig.BUILD_TYPE) {
                 case "debug":
-                    VERSION_RELEASE = "1.2.400";
+                    VERSION_RELEASE = "1.2.450";
                     break;
                 default:
                     VERSION_RELEASE = BuildConfig.VERSION_NAME;
