4c5ef5e2f301df5fc72de19abbc6c868821f0c76
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2020-05-17 08:40:40 +0530

    Title: Merchant-Queue: create token with username added

--- merchant/src/queue/java/com/noqapp/android/merchant/views/fragments/MerchantDetailFragment.java
+++ merchant/src/queue/java/com/noqapp/android/merchant/views/fragments/MerchantDetailFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -14,16 +15,22 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.hbb20.CountryCodePicker;
 import com.noqapp.android.common.beans.ErrorEncounteredJson;
+import com.noqapp.android.common.beans.JsonProfile;
 import com.noqapp.android.common.beans.store.JsonPurchaseOrder;
 import com.noqapp.android.common.beans.store.JsonPurchaseOrderList;
 import com.noqapp.android.common.customviews.CustomToast;
@@ -35,12 +42,16 @@ import com.noqapp.android.common.model.types.QueueStatusEnum;
 import com.noqapp.android.common.model.types.UserLevelEnum;
 import com.noqapp.android.common.model.types.order.PurchaseOrderStateEnum;
 import com.noqapp.android.common.utils.Formatter;
+import com.noqapp.android.common.utils.PhoneFormatterUtil;
 import com.noqapp.android.merchant.R;
+import com.noqapp.android.merchant.model.BusinessCustomerApiCalls;
 import com.noqapp.android.merchant.presenter.beans.JsonBusinessCustomer;
+import com.noqapp.android.merchant.presenter.beans.JsonBusinessCustomerLookup;
 import com.noqapp.android.merchant.presenter.beans.JsonQueuedPerson;
 import com.noqapp.android.merchant.presenter.beans.JsonToken;
 import com.noqapp.android.merchant.presenter.beans.JsonTopic;
 import com.noqapp.android.merchant.presenter.beans.body.merchant.OrderServed;
+import com.noqapp.android.merchant.utils.AppUtils;
 import com.noqapp.android.merchant.utils.ErrorResponseHandler;
 import com.noqapp.android.merchant.utils.IBConstant;
 import com.noqapp.android.merchant.utils.ShowAlertInformation;
@@ -53,8 +64,10 @@ import com.noqapp.android.merchant.views.activities.OrderDetailActivity;
 import com.noqapp.android.merchant.views.activities.ProductListActivity;
 import com.noqapp.android.merchant.views.activities.StoreMenuActivity;
 import com.noqapp.android.merchant.views.activities.ViewAllPeopleInQOrderActivity;
+import com.noqapp.android.merchant.views.adapters.JsonProfileAdapter;
 import com.noqapp.android.merchant.views.adapters.PeopleInQOrderAdapter;
 import com.noqapp.android.merchant.views.interfaces.AcquireOrderPresenter;
+import com.noqapp.android.merchant.views.interfaces.FindCustomerPresenter;
 import com.noqapp.android.merchant.views.interfaces.OrderProcessedPresenter;
 import com.noqapp.android.merchant.views.interfaces.PurchaseOrderPresenter;
 import com.noqapp.android.merchant.views.model.PurchaseOrderApiCalls;
@@ -69,11 +82,18 @@ import java.util.List;
 public class MerchantDetailFragment extends BaseMerchantDetailFragment implements
         PurchaseOrderPresenter, AcquireOrderPresenter, OrderProcessedPresenter,
         PeopleInQOrderAdapter.PeopleInQOrderAdapterClick, OrderDetailActivity.UpdateWholeList,
-        HCSMenuActivity.UpdateWholeList, StoreMenuActivity.UpdateWholeList {
+        HCSMenuActivity.UpdateWholeList, StoreMenuActivity.UpdateWholeList, FindCustomerPresenter {
 
     private PeopleInQOrderAdapter peopleInQOrderAdapter;
     private List<JsonPurchaseOrder> purchaseOrders = new ArrayList<>();
-
+    private Spinner sp_patient_list;
+    private TextView tv_select_patient;
+    private Button btn_create_order;
+    private BusinessCustomerApiCalls businessCustomerApiCalls;
+    private String countryCode = "";
+    private String cid = "";
+    private CountryCodePicker ccp;
+    private long mLastClickTime = 0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
@@ -117,7 +137,8 @@ public class MerchantDetailFragment extends BaseMerchantDetailFragment implement
                 ((Activity) context).startActivity(intent);
             }
         } else {
-            showCreateTokenDialog(context, codeQR);
+            //showCreateTokenDialog(context, codeQR);
+            showCreateTokenDialogWithMobile(context, codeQR);
         }
     }
 
@@ -625,5 +646,160 @@ public class MerchantDetailFragment extends BaseMerchantDetailFragment implement
         } else {
             menuItem.setTitle("Create Token");
         }
+    }
+
+
+    private void showCreateTokenDialogWithMobile(final Context mContext, final String codeQR) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        builder.setTitle(null);
+        View view = inflater.inflate(R.layout.dialog_create_token_with_mobile, null, false);
+        ImageView actionbarBack = view.findViewById(R.id.actionbarBack);
+        tv_create_token = view.findViewById(R.id.tvtitle);
+        tvCount = view.findViewById(R.id.tvcount);
+        ll_main_section = view.findViewById(R.id.ll_main_section);
+        ll_mobile = view.findViewById(R.id.ll_mobile);
+        edt_mobile = view.findViewById(R.id.edt_mobile);
+        sp_patient_list = view.findViewById(R.id.sp_patient_list);
+        tv_select_patient = view.findViewById(R.id.tv_select_patient);
+
+
+        final EditText edt_id = view.findViewById(R.id.edt_id);
+        final RadioGroup rg_user_id = view.findViewById(R.id.rg_user_id);
+        final RadioButton rb_mobile = view.findViewById(R.id.rb_mobile);
+        builder.setView(view);
+        final AlertDialog mAlertDialog = builder.create();
+        mAlertDialog.setCanceledOnTouchOutside(false);
+        rg_user_id.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rb_mobile) {
+                    ll_mobile.setVisibility(View.VISIBLE);
+                    edt_id.setVisibility(View.GONE);
+                    edt_id.setText("");
+                } else {
+                    edt_id.setVisibility(View.VISIBLE);
+                    ll_mobile.setVisibility(View.GONE);
+                    edt_mobile.setText("");
+                }
+            }
+        });
+        cid = "";
+        ccp = view.findViewById(R.id.ccp);
+        String c_codeValue = LaunchActivity.getLaunchActivity().getUserProfile().getCountryShortName();
+        int c_code = PhoneFormatterUtil.getCountryCodeFromRegion(c_codeValue.toUpperCase());
+        ccp.setDefaultCountryUsingNameCode(String.valueOf(c_code));
+        btn_create_order = view.findViewById(R.id.btn_create_order);
+        btn_create_token = view.findViewById(R.id.btn_create_token);
+        btn_create_token.setText("Search Customer");
+        btn_create_token.setOnClickListener(v -> {
+            if (SystemClock.elapsedRealtime() - mLastClickTime < 3000) {
+                return;
+            }
+            mLastClickTime = SystemClock.elapsedRealtime();
+            boolean isValid = true;
+            edt_mobile.setError(null);
+            edt_id.setError(null);
+            AppUtils.hideKeyBoard(getActivity());
+            // get selected radio button from radioGroup
+            int selectedId = rg_user_id.getCheckedRadioButtonId();
+            if (selectedId == R.id.rb_mobile) {
+                if (TextUtils.isEmpty(edt_mobile.getText())) {
+                    edt_mobile.setError(getString(R.string.error_mobile_blank));
+                    isValid = false;
+                }
+            } else {
+                if (TextUtils.isEmpty(edt_id.getText())) {
+                    edt_id.setError(getString(R.string.error_customer_id));
+                    isValid = false;
+                }
+            }
+
+
+            if (isValid) {
+                setProgressMessage("Searching customer...");
+                showProgress();
+                setProgressCancel(false);
+                setDispensePresenter();
+                String phone = "";
+                cid = "";
+                if (rb_mobile.isChecked()) {
+                    edt_id.setText("");
+                    countryCode = ccp.getSelectedCountryCode();
+                    phone = countryCode + edt_mobile.getText().toString();
+                    cid = "";
+                } else {
+                    cid = edt_id.getText().toString();
+                    edt_mobile.setText("");// set blank so that wrong phone no not pass to login screen
+                }
+                businessCustomerApiCalls = new BusinessCustomerApiCalls();
+                businessCustomerApiCalls.setFindCustomerPresenter(MerchantDetailFragment.this);
+                businessCustomerApiCalls.findCustomer(
+                        BaseLaunchActivity.getDeviceID(),
+                        LaunchActivity.getLaunchActivity().getEmail(),
+                        LaunchActivity.getLaunchActivity().getAuth(),
+                        new JsonBusinessCustomerLookup().setCodeQR(codeQR).setCustomerPhone(phone).setBusinessCustomerId(cid));
+                btn_create_token.setClickable(false);
+                //  mAlertDialog.dismiss();
+            }
+        });
+
+        actionbarBack.setOnClickListener(v -> mAlertDialog.dismiss());
+        mAlertDialog.show();
+    }
+
+
+    @Override
+    public void findCustomerResponse(JsonProfile jsonProfile) {
+        dismissProgress();
+        mLastClickTime = 0;
+        if (null != jsonProfile) {
+            List<JsonProfile> jsonProfileList = new ArrayList<>();
+            jsonProfileList.add(jsonProfile);
+            if (jsonProfile.getDependents().size() > 0) {
+                jsonProfileList.addAll(jsonProfile.getDependents());
+            }
+            JsonProfileAdapter adapter = new JsonProfileAdapter(getActivity(), jsonProfileList);
+            sp_patient_list.setAdapter(adapter);
+            sp_patient_list.setVisibility(View.VISIBLE);
+            sp_patient_list.setEnabled(false);
+            edt_mobile.setEnabled(false);
+            tv_select_patient.setVisibility(View.VISIBLE);
+            tv_select_patient.setText("Customer Name");
+            btn_create_order.setVisibility(View.VISIBLE);
+            btn_create_token.setVisibility(View.GONE);
+            btn_create_order.setOnClickListener(v -> {
+                if (LaunchActivity.getLaunchActivity().isOnline()) {
+                    if (SystemClock.elapsedRealtime() - mLastClickTime < 3000) {
+                        return;
+                    }
+                    mLastClickTime = SystemClock.elapsedRealtime();
+                    btn_create_order.setEnabled(false);
+                    setProgressMessage("Creating token...");
+                    showProgress();
+                    setProgressCancel(false);
+                    String phoneNoWithCode = "";
+                    setDispensePresenter();
+                    if (TextUtils.isEmpty(cid)) {
+                        phoneNoWithCode = PhoneFormatterUtil.phoneNumberWithCountryCode(jsonProfile.getPhoneRaw(), jsonProfile.getCountryShortName());
+                    }
+
+                    JsonBusinessCustomer jsonBusinessCustomer = new JsonBusinessCustomer().
+                            setQueueUserId(jsonProfile.getQueueUserId());
+                    jsonBusinessCustomer
+                            .setCodeQR(topicsList.get(currentPosition).getCodeQR())
+                            .setCustomerPhone(phoneNoWithCode)
+                            .setBusinessCustomerId(cid);
+                    manageQueueApiCalls.dispenseTokenWithClientInfo(
+                            BaseLaunchActivity.getDeviceID(),
+                            LaunchActivity.getLaunchActivity().getEmail(),
+                            LaunchActivity.getLaunchActivity().getAuth(),
+                            jsonBusinessCustomer);
+
+                } else {
+                    ShowAlertInformation.showNetworkDialog(getActivity());
+                }
+            });
+        }
+
     }
 }
