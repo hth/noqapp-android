e11a9f993e6dd86a8e755d829ce9e1584844ccc5
hth <hth@users.noreply.github.com>
2020-06-13 15:33:12 -0700

    Added API to send message to customer

---
+++ merchant/src/main/java/com/noqapp/android/merchant/model/response/api/MessageCustomerApiUrls.java
@@ -0,0 +1,35 @@
+package com.noqapp.android.merchant.model.response.api;
+
+import com.noqapp.android.common.beans.JsonResponse;
+import com.noqapp.android.merchant.presenter.beans.body.merchant.MessageCustomer;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface MessageCustomerApiUrls {
+
+    /**
+     * Errors
+     * {@link javax.servlet.http.HttpServletResponse#SC_UNAUTHORIZED} - HTTP STATUS 401
+     * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#SEVERE}
+     */
+    @POST("api/m/message/customer.json")
+    Call<JsonResponse> messageCustomer(
+            @Header("X-R-DID")
+            String did,
+
+            @Header("X-R-DT")
+            String dt,
+
+            @Header("X-R-MAIL")
+            String mail,
+
+            @Header("X-R-AUTH")
+            String auth,
+
+            @Body
+            MessageCustomer messageCustomer
+    );
+}
