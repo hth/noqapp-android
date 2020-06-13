48adf9d21f94e5ac9e7eb6d4c09d78196f972611
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2020-06-13 09:32:11 +0530

    Title : Merchant - hide pwd image added

--- merchant/src/main/java/com/noqapp/android/merchant/views/fragments/LoginFragment.java
+++ merchant/src/main/java/com/noqapp/android/merchant/views/fragments/LoginFragment.java
@@ -119,12 +119,12 @@ public class LoginFragment extends BaseFragment implements LoginPresenter, Merch
         ImageView iv_show_hide_password = view.findViewById(R.id.iv_show_hide_password);
         iv_show_hide_password.setOnClickListener(v -> {
             if (edt_pwd.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
-                iv_show_hide_password.setImageResource(R.drawable.show_password);
+                iv_show_hide_password.setImageResource(R.drawable.password_hide);
                 //@TODO change image with close/cross eye
                 //Show Password
                 edt_pwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
             } else {
-                iv_show_hide_password.setImageResource(R.drawable.show_password);
+                iv_show_hide_password.setImageResource(R.drawable.password_show);
                 //Hide Password
                 edt_pwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
