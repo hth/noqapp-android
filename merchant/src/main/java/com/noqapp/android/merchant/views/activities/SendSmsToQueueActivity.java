15a910167c879f07051bcb55df05a08a7f8f3e97
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2020-06-14 13:43:27 +0530

    Title: Merchant - SMS screen validation updated

--- merchant/src/main/java/com/noqapp/android/merchant/views/activities/SendSmsToQueueActivity.java
+++ merchant/src/main/java/com/noqapp/android/merchant/views/activities/SendSmsToQueueActivity.java
@@ -7,20 +7,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-
 import com.noqapp.android.common.beans.ErrorEncounteredJson;
 import com.noqapp.android.common.beans.JsonResponse;
 import com.noqapp.android.common.customviews.CustomToast;
 import com.noqapp.android.merchant.R;
 import com.noqapp.android.merchant.model.MessageCustomerApiCalls;
-import com.noqapp.android.merchant.presenter.beans.JsonToken;
 import com.noqapp.android.merchant.presenter.beans.JsonTopic;
 import com.noqapp.android.merchant.presenter.beans.body.merchant.MessageCustomer;
 import com.noqapp.android.merchant.utils.AppUtils;
 import com.noqapp.android.merchant.utils.Constants;
 import com.noqapp.android.merchant.utils.ErrorResponseHandler;
 import com.noqapp.android.merchant.utils.UserUtils;
-import com.noqapp.android.merchant.views.adapters.JsonProfileAdapter;
 import com.noqapp.android.merchant.views.adapters.JsonTopicAdapter;
 import com.noqapp.android.merchant.views.interfaces.MessageCustomerPresenter;
 
@@ -30,6 +27,7 @@ public class SendSmsToQueueActivity extends BaseActivity implements MessageCusto
 
     private Spinner sp_queue;
     private ArrayList<JsonTopic> jsonTopics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +36,7 @@ public class SendSmsToQueueActivity extends BaseActivity implements MessageCusto
         tv_toolbar_title.setText("Send SMS to Queues");
         Button btn_submit = findViewById(R.id.btn_submit);
         sp_queue = findViewById(R.id.sp_queue);
-        // max character 500 limit
+        // max character 256 limit
         final EditText edt_body = findViewById(R.id.edt_body);
         final EditText edt_subject = findViewById(R.id.edt_subject);
         Bundle extras = getIntent().getExtras();
@@ -56,9 +54,11 @@ public class SendSmsToQueueActivity extends BaseActivity implements MessageCusto
             AppUtils.hideKeyBoard(SendSmsToQueueActivity.this);
             edt_body.setError(null);
             if (TextUtils.isEmpty(edt_subject.getText().toString())) {
-                edt_subject.setError(getString(R.string.error_subject_blank));
+                edt_subject.setError(getString(R.string.error_title_blank));
+            } else if (!TextUtils.isEmpty(edt_subject.getText().toString()) && edt_subject.getText().toString().length() < 4) {
+                edt_subject.setError(getString(R.string.error_title_min_length));
             } else if (TextUtils.isEmpty(edt_body.getText().toString())) {
-                edt_body.setError(getString(R.string.error_feedback_blank));
+                edt_body.setError(getString(R.string.error_body_blank));
             } else {
                 setProgressMessage("Sending sms...");
                 showProgress();
@@ -77,14 +77,14 @@ public class SendSmsToQueueActivity extends BaseActivity implements MessageCusto
 
     }
 
-    private ArrayList<String> getSelectedCodeQRList(){
+    private ArrayList<String> getSelectedCodeQRList() {
         ArrayList<String> data = new ArrayList<>();
-        if(sp_queue.getSelectedItemPosition() == jsonTopics.size()-1){
+        if (sp_queue.getSelectedItemPosition() == jsonTopics.size() - 1) {
             // Do not send the added JsonTopic for all
-            for (int i = 0; i < jsonTopics.size() -1 ; i++) {
+            for (int i = 0; i < jsonTopics.size() - 1; i++) {
                 data.add(jsonTopics.get(i).getCodeQR());
             }
-        }else{
+        } else {
             data.add(jsonTopics.get(sp_queue.getSelectedItemPosition()).getCodeQR());
         }
         return data;
