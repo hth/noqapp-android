7265f9873fcf749cb10a7006f7bf64486648dd2b
hth <hth@users.noreply.github.com>
2020-06-15 06:49:33 -0700

    Clean up

--- merchant/src/main/java/com/noqapp/android/merchant/views/interfaces/ApproveCustomerPresenter.java
+++ merchant/src/main/java/com/noqapp/android/merchant/views/interfaces/ApproveCustomerPresenter.java
@@ -1,6 +1,5 @@
 package com.noqapp.android.merchant.views.interfaces;
 
-import com.noqapp.android.common.beans.JsonResponse;
 import com.noqapp.android.common.presenter.ResponseErrorPresenter;
 import com.noqapp.android.merchant.presenter.beans.JsonQueuedPerson;
 
