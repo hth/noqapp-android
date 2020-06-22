86c25ff86d3843594c70210c234fe47b6285bed8
hth <hth@users.noreply.github.com>
2020-06-22 15:05:21 -0700

    Added force a crash (revert)

--- client/src/main/java/com/noqapp/android/client/views/activities/LaunchActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/LaunchActivity.java
@@ -24,6 +24,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ExpandableListView;
@@ -280,6 +281,21 @@ public class LaunchActivity
             }
         }
         setKioskMode();
+        forceACrash();
+    }
+
+    public void forceACrash() {
+        Button crashButton = new Button(this);
+        crashButton.setText("Crash!");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                throw new RuntimeException("Test Crash"); // Force a crash
+            }
+        });
+
+        addContentView(crashButton, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 
     private void setKioskMode() {
