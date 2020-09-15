685ed9c898a8a6870380ad8c221526418edac3bc
hth <hth@users.noreply.github.com>
2020-09-15 11:00:58 -0700

    Fixed text

--- client/src/main/java/com/noqapp/android/client/views/adapters/CurrentActivityAdapter.java
+++ client/src/main/java/com/noqapp/android/client/views/adapters/CurrentActivityAdapter.java
@@ -57,7 +57,7 @@ public class CurrentActivityAdapter extends RecyclerView.Adapter {
             final JsonTokenAndQueue jsonTokenAndQueue = (JsonTokenAndQueue) object;
             holder.tv_name.setText(jsonTokenAndQueue.getDisplayName());
             holder.card_view.setOnClickListener((View v) -> listener.currentQorOrderItemClick(jsonTokenAndQueue));
-            holder.tv_position_in_queue_value.setText(jsonTokenAndQueue.afterHowLongForDisplay());
+            holder.tv_position_in_queue_value.setText(String.valueOf(jsonTokenAndQueue.afterHowLong()));
             if (QueueOrderTypeEnum.Q == jsonTokenAndQueue.getBusinessType().getQueueOrderType()) {
                 holder.tv_token_label.setText(context.getString(R.string.token));
                 if (jsonTokenAndQueue.getToken() <= jsonTokenAndQueue.getServingNumber()) {
