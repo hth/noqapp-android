45dc5c2adad5424920e0f0294d763ec70bcd0cdf
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2020-09-17 14:03:07 +0530

    Title: Client - Temple UI added; temple list UI updated

--- client/src/main/java/com/noqapp/android/client/views/adapters/LevelUpQueueAdapter.java
+++ client/src/main/java/com/noqapp/android/client/views/adapters/LevelUpQueueAdapter.java
@@ -131,6 +131,7 @@ public class LevelUpQueueAdapter extends BaseExpandableListAdapter {
             childViewHolder.tv_name.setText(bizStoreElastic.getDisplayName());
             childViewHolder.tv_store_rating.setText(String.valueOf(AppUtils.round(bizStoreElastic.getRating())));
             childViewHolder.tv_address.setText(AppUtils.getStoreAddress(bizStoreElastic.getTown(), bizStoreElastic.getArea()));
+            childViewHolder.tv_address.setVisibility(View.GONE);
             AppUtils.setReviewCountText(bizStoreElastic.getReviewCount(), childViewHolder.tv_store_review);
             childViewHolder.tv_store_review.setOnClickListener((View v) -> {
                 if (bizStoreElastic.getReviewCount() > 0) {
@@ -143,7 +144,9 @@ public class LevelUpQueueAdapter extends BaseExpandableListAdapter {
                     context.startActivity(in);
                 }
             });
-            childViewHolder.tv_specialization.setText(bizStoreElastic.getCompleteEducation());
+            String specialization = bizStoreElastic.getCompleteEducation();
+            childViewHolder.tv_specialization.setText(specialization);
+            childViewHolder.tv_specialization.setVisibility(TextUtils.isEmpty(specialization) ? View.GONE : View.VISIBLE);
             StoreHourElastic storeHourElastic = AppUtils.getStoreHourElastic(bizStoreElastic.getStoreHourElasticList());
             childViewHolder.tv_join.setEnabled(!storeHourElastic.isDayClosed());
             if (storeHourElastic.isDayClosed()) {
@@ -247,7 +250,6 @@ public class LevelUpQueueAdapter extends BaseExpandableListAdapter {
                 childViewHolder.tv_status.setTextColor(context.getResources().getColor(R.color.button_color));
             }
             AppUtils.loadProfilePic(childViewHolder.iv_main, bizStoreElastic.getDisplayImage(), context);
-            childViewHolder.tv_consult_fees.setVisibility(bizStoreElastic.getProductPrice() == 0 ? View.GONE : View.VISIBLE);
             if (bizStoreElastic.getProductPrice() == 0) {
                 childViewHolder.tv_consult_fees.setVisibility(View.GONE);
                 childViewHolder.tv_consult_fees_header.setVisibility(View.GONE);
@@ -258,7 +260,9 @@ public class LevelUpQueueAdapter extends BaseExpandableListAdapter {
                 childViewHolder.tv_consult_fees.setVisibility(View.VISIBLE);
                 childViewHolder.tv_consult_fees_header.setVisibility(View.VISIBLE);
             }
-            childViewHolder.tv_store_special.setText(bizStoreElastic.getFamousFor());
+            String special = bizStoreElastic.getFamousFor();
+            childViewHolder.tv_store_special.setText(special);
+            childViewHolder.tv_store_special.setVisibility(TextUtils.isEmpty(special) ? View.GONE : View.VISIBLE);
             // for safety null check added for walking state
             if (null == bizStoreElastic.getWalkInState() || bizStoreElastic.getWalkInState() == WalkInStateEnum.E) {
                 childViewHolder.tv_join.setBackground(ContextCompat.getDrawable(context, R.drawable.orange_gradient));
@@ -280,6 +284,12 @@ public class LevelUpQueueAdapter extends BaseExpandableListAdapter {
             switch (bizStoreElastic.getBusinessType()) {
                 case DO:
                 case PW:
+                    childViewHolder.tv_specialization.setVisibility(View.GONE);
+                    childViewHolder.tv_store_rating.setVisibility(View.GONE);
+                    childViewHolder.tv_store_review.setVisibility(View.GONE);
+                    childViewHolder.tv_store_special.setVisibility(View.GONE);
+                    childViewHolder.tv_consult_fees.setVisibility(View.GONE);
+                    childViewHolder.tv_consult_fees_header.setVisibility(View.GONE);
                     switch (bizStoreElastic.getAppointmentState()) {
                         case O:
                             childViewHolder.btn_book_appointment.setVisibility(View.GONE);
