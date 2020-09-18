14c11615cf3e75d45a8f87866ad8e7dea4106f4b
hth <hth@users.noreply.github.com>
2020-09-17 23:08:28 -0700

    Fixed missing implementation

--- merchant/src/tv/java/com/noqapp/android/merchant/views/pojos/PreferenceObjects.java
+++ merchant/src/tv/java/com/noqapp/android/merchant/views/pojos/PreferenceObjects.java
@@ -1,4 +1,21 @@
 package com.noqapp.android.merchant.views.pojos;
 
+import com.google.gson.annotations.SerializedName;
+import com.noqapp.android.merchant.utils.Constants;
+
+import java.util.ArrayList;
+
 public class PreferenceObjects {
+
+    @SerializedName(Constants.SYMPTOMS)
+    private ArrayList<DataObj> symptomsList = new ArrayList<>();
+
+    public ArrayList<DataObj> getSymptomsList() {
+        return symptomsList;
+    }
+
+    public PreferenceObjects setSymptomsList(ArrayList<DataObj> symptomsList) {
+        this.symptomsList = symptomsList;
+        return this;
+    }
 }
