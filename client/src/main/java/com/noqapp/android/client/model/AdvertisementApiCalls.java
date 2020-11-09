8691e0325f4c1cf4e3904059c0d6be44bc505893
hth <hth@users.noreply.github.com>
2020-11-08 22:34:49 -0800

    Removed deprecated method

--- client/src/main/java/com/noqapp/android/client/model/AdvertisementApiCalls.java
+++ client/src/main/java/com/noqapp/android/client/model/AdvertisementApiCalls.java
@@ -29,37 +29,6 @@ public class AdvertisementApiCalls {
         advertisementMobileApiUrls = RetrofitClient.getClient().create(AdvertisementMobileApiUrls.class);
     }
 
-    public void getAllAdvertisements(String did) {
-        advertisementMobileApiUrls.getAllAdvertisements(did, Constants.DEVICE_TYPE).enqueue(new Callback<JsonAdvertisementList>() {
-            @Override
-            public void onResponse(@NonNull Call<JsonAdvertisementList> call, @NonNull Response<JsonAdvertisementList> response) {
-                if (response.code() == Constants.SERVER_RESPONSE_CODE_SUCCESS) {
-                    if (null != response.body() && null == response.body().getError()) {
-                        advertisementPresenter.advertisementResponse(response.body());
-                        Log.d("getAllAdvertisements", String.valueOf(response.body()));
-                    } else {
-                        Log.d(TAG, "Empty all Advertisements");
-                        advertisementPresenter.responseErrorPresenter(response.body().getError());
-                    }
-                } else {
-                    if (response.code() == Constants.INVALID_CREDENTIAL) {
-                        advertisementPresenter.authenticationFailure();
-                    } else {
-                        advertisementPresenter.responseErrorPresenter(response.code());
-                        Log.e(TAG, "" + response.code());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<JsonAdvertisementList> call, @NonNull Throwable t) {
-                Log.e("onFailure Advt", t.getLocalizedMessage(), t);
-                advertisementPresenter.responseErrorPresenter(null);
-            }
-        });
-    }
-
-
     public void getAdvertisementsByLocation(String did, Location location) {
         advertisementMobileApiUrls.getAdvertisementsByLocation(did, Constants.DEVICE_TYPE, location).enqueue(new Callback<JsonAdvertisementList>() {
             @Override
--- client/src/main/java/com/noqapp/android/client/model/response/open/AdvertisementMobileApiUrls.java
+++ client/src/main/java/com/noqapp/android/client/model/response/open/AdvertisementMobileApiUrls.java
@@ -5,7 +5,6 @@ import com.noqapp.android.common.beans.JsonAdvertisementList;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
@@ -14,20 +13,6 @@ import retrofit2.http.POST;
  * 2019-05-20 11:03
  */
 public interface AdvertisementMobileApiUrls {
-
-    /**
-     * ERROR
-     * {@link com.noqapp.android.common.model.types.MobileSystemErrorCodeEnum#SEVERE}
-     */
-    @GET("open/vigyapan/all.json")
-    Call<JsonAdvertisementList> getAllAdvertisements(
-        @Header("X-R-DID")
-        String did,
-
-        @Header("X-R-DT")
-        String dt
-    );
-
 
     /**
      * ERROR
