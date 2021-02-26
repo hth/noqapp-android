b08d943de5e4a59d25fe9d4ff89abf933865ea4c
ChandraSharma0377 <ChandraSharma0377@users.noreply.github.com>
2021-02-26 09:24:22 +0530

    Title: Merchant- Notification api call added

--- merchant/src/main/java/com/noqapp/android/merchant/model/NotificationApiCall.java
+++ merchant/src/main/java/com/noqapp/android/merchant/model/NotificationApiCall.java
@@ -1,12 +1,12 @@
-package com.noqapp.android.client.model;
+package com.noqapp.android.merchant.model;
 
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.noqapp.android.client.model.response.api.NotificationApiUrls;
-import com.noqapp.android.client.network.RetrofitClient;
-import com.noqapp.android.client.utils.Constants;
+import com.noqapp.android.merchant.model.response.api.NotificationApiUrls;
+import com.noqapp.android.merchant.network.RetrofitClient;
+import com.noqapp.android.merchant.utils.Constants;
 import com.noqapp.android.common.beans.JsonResponse;
 import com.noqapp.android.common.beans.body.Notification;
 import com.noqapp.android.common.presenter.NotificationPresenter;
@@ -18,7 +18,6 @@ import retrofit2.Response;
 public class NotificationApiCall {
     private final String TAG = NotificationApiCall.class.getSimpleName();
     private static final NotificationApiUrls notificationApiUrls;
-    private static final com.noqapp.android.client.model.response.open.NotificationApiUrls notificationApiUrlsOpen;
     private NotificationPresenter notificationPresenter;
 
     public NotificationApiCall(NotificationPresenter notificationPresenter) {
@@ -27,7 +26,6 @@ public class NotificationApiCall {
 
     static {
         notificationApiUrls = RetrofitClient.getClient().create(NotificationApiUrls.class);
-        notificationApiUrlsOpen = RetrofitClient.getClient().create(com.noqapp.android.client.model.response.open.NotificationApiUrls.class);
     }
 
     public void notificationViewed(String did, String mail, String auth, Notification notification) {
@@ -59,32 +57,4 @@ public class NotificationApiCall {
         });
     }
 
-    public void notificationViewed(String did, Notification notification) {
-        notificationApiUrlsOpen.notificationViewed(did, Constants.DEVICE_TYPE, notification).enqueue(new Callback<JsonResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<JsonResponse> call, @NonNull Response<JsonResponse> response) {
-                if (response.code() == Constants.SERVER_RESPONSE_CODE_SUCCESS) {
-                    if (null != response.body() && null == response.body().getError()) {
-                        Log.d("Response notification", String.valueOf(response.body()));
-                        notificationPresenter.notificationResponse(response.body());
-                    } else {
-                        Log.e(TAG, "Error notification" + response.body().getError());
-                        notificationPresenter.responseErrorPresenter(response.body().getError());
-                    }
-                } else {
-                    if (response.code() == Constants.INVALID_CREDENTIAL) {
-                        notificationPresenter.authenticationFailure();
-                    } else {
-                        notificationPresenter.responseErrorPresenter(response.code());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<JsonResponse> call, @NonNull Throwable t) {
-                Log.e("Failure notification", t.getLocalizedMessage(), t);
-                notificationPresenter.responseErrorPresenter(null);
-            }
-        });
-    }
 }
