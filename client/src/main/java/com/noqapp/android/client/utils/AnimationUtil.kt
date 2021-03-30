3fd9a11aecc46c376185217e8deeb712bfdac2e5
vivekthengha <vivek.g621@gmail.com>
2021-03-30 13:49:14 +0530

    fix issues

--- client/src/main/java/com/noqapp/android/client/utils/AnimationUtil.kt
+++ client/src/main/java/com/noqapp/android/client/utils/AnimationUtil.kt
@@ -3,6 +3,11 @@ package com.noqapp.android.client.utils
 import android.animation.ObjectAnimator
 import android.view.View
 
+
+/**
+ * utility class for animations
+ * Created by Vivek Jha on 23/03/2021
+ */
 class AnimationUtil {
 
     companion object {
