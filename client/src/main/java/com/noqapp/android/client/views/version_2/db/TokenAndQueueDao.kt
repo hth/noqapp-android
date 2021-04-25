60667c89c43c6d772c269a2b7e166dab9bc959cd
hth <hth@users.noreply.github.com>
2021-04-24 19:40:02 -0700

    Updated field name

--- client/src/main/java/com/noqapp/android/client/views/version_2/db/TokenAndQueueDao.kt
+++ client/src/main/java/com/noqapp/android/client/views/version_2/db/TokenAndQueueDao.kt
@@ -9,22 +9,22 @@ import com.noqapp.android.client.presenter.beans.JsonTokenAndQueue
 @Dao
 interface TokenAndQueueDao {
 
-    @Query("SELECT * FROM token_queue WHERE historyQueue!=1 ORDER BY create_date ASC ")
+    @Query("SELECT * FROM token_queue WHERE history_queue!=1 ORDER BY create_date ASC ")
     fun getCurrentQueueList(): LiveData<List<JsonTokenAndQueue>>
 
-    @Query("SELECT * FROM token_queue WHERE historyQueue!=1 AND qr_code=:qrCode AND token=:token")
+    @Query("SELECT * FROM token_queue WHERE history_queue!=1 AND qr_code=:qrCode AND token=:token")
     fun getCurrentQueueObject(qrCode: String, token: Int): LiveData<JsonTokenAndQueue>
 
-    @Query("SELECT * FROM token_queue WHERE historyQueue!=1 AND qr_code=:qrCode")
+    @Query("SELECT * FROM token_queue WHERE history_queue!=1 AND qr_code=:qrCode")
     fun findByQRCode(qrCode: String): LiveData<JsonTokenAndQueue>
 
-    @Query("SELECT * FROM token_queue WHERE historyQueue!=1 AND qr_code=:qrCode")
+    @Query("SELECT * FROM token_queue WHERE history_queue!=1 AND qr_code=:qrCode")
     fun getCurrentQueueObjectList(qrCode: String?): LiveData<List<JsonTokenAndQueue>>
 
-    @Query("SELECT * FROM token_queue WHERE historyQueue=1 AND qr_code=:qrCode AND token=:token")
+    @Query("SELECT * FROM token_queue WHERE history_queue=1 AND qr_code=:qrCode AND token=:token")
     fun getHistoryQueueObject(qrCode: String, token: Int): LiveData<JsonTokenAndQueue>
 
-    @Query("SELECT * , MAX(create_date) FROM token_queue WHERE historyQueue=1 GROUP BY qr_code")
+    @Query("SELECT * , MAX(create_date) FROM token_queue WHERE history_queue=1 GROUP BY qr_code")
     fun getHistoryQueueList(): LiveData<List<JsonTokenAndQueue>>
 
     @Insert
@@ -36,16 +36,16 @@ interface TokenAndQueueDao {
     @Query("UPDATE token_queue SET serving_number=:servingNumber, display_serving_number=:displayServingNumber WHERE qr_code=:qrCode AND token=:token")
     fun updateCurrentListQueueObject(qrCode: String?, servingNumber: String, displayServingNumber: String, token: Int)
 
-    @Query("UPDATE token_queue SET puchase_order_state=:orderState WHERE qr_code=:qrCode AND token=:token")
+    @Query("UPDATE token_queue SET purchase_order_state=:orderState WHERE qr_code=:qrCode AND token=:token")
     fun updateCurrentListOrderObject(qrCode: String?, orderState: String, token: Int)
 
-    @Query("DELETE FROM token_queue WHERE historyQueue!=1")
+    @Query("DELETE FROM token_queue WHERE history_queue!=1")
     fun deleteCurrentQueue()
 
-    @Query("DELETE FROM token_queue WHERE historyQueue=1")
+    @Query("DELETE FROM token_queue WHERE history_queue=1")
     fun deleteHistoryQueue()
 
-    @Query("DELETE FROM token_queue WHERE historyQueue!=1 AND qr_code=:qrCode AND token=:token")
+    @Query("DELETE FROM token_queue WHERE history_queue!=1 AND qr_code=:qrCode AND token=:token")
     fun deleteTokenQueue(qrCode: String, token: Int)
 
 }
