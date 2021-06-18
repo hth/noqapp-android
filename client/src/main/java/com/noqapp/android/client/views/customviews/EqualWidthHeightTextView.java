783d1bb618f8f4d9ed220086e1e794d5c1793402
hth <hth@users.noreply.github.com>
2021-06-18 13:39:18 -0700

    Changed blinker

---
+++ client/src/main/java/com/noqapp/android/client/views/customviews/EqualWidthHeightTextView.java
@@ -0,0 +1,28 @@
+package com.noqapp.android.client.views.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class EqualWidthHeightTextView extends TextView {
+
+    public EqualWidthHeightTextView(Context context) {
+        super(context);
+    }
+
+    public EqualWidthHeightTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EqualWidthHeightTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int r = Math.max(getMeasuredWidth(),getMeasuredHeight());
+        setMeasuredDimension(r, r);
+    }
+}
