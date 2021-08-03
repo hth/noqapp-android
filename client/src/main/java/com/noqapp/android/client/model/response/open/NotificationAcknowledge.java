87d521f32f03fad7dce0e4c724c3bb1e63045c43
hth <hth@users.noreply.github.com>
2021-08-02 21:54:30 -0700

    Renamed

--- client/src/main/java/com/noqapp/android/client/model/response/open/NotificationAcknowledge.java
+++ client/src/main/java/com/noqapp/android/client/model/response/open/NotificationAcknowledge.java
@@ -8,7 +8,7 @@ import retrofit2.http.Body;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
-public interface NotificationApiUrls {
+public interface NotificationAcknowledge {
 
     /**
      * Errors
