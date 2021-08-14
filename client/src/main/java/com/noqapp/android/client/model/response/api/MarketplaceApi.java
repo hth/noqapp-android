4bed75d2d277e2b7f7911c5807099d30b7b29e51
hth <hth@users.noreply.github.com>
2021-08-15 04:38:18 +0530

    Added more methods to view detail and inititate contact with owner of post

--- client/src/main/java/com/noqapp/android/client/model/response/api/MarketplaceApi.java
+++ client/src/main/java/com/noqapp/android/client/model/response/api/MarketplaceApi.java
@@ -1,7 +1,7 @@
 package com.noqapp.android.client.model.response.api;
 
-import com.noqapp.android.client.presenter.beans.BizStoreElasticList;
-import com.noqapp.android.client.presenter.beans.body.SearchQuery;
+import com.noqapp.android.common.beans.JsonResponse;
+import com.noqapp.android.common.beans.marketplace.JsonMarketplace;
 import com.noqapp.android.common.beans.marketplace.MarketplaceElasticList;
 
 import retrofit2.Call;
@@ -29,5 +29,49 @@ public interface MarketplaceApi {
 
         @Header("X-R-AUTH")
         String auth
+    );
+
+    /**
+     * Errors
+     * {@link com.noqapp.android.common.beans.JsonResponse#response} is false(0) when not found
+     */
+    @POST("api/c/view")
+    Call<JsonResponse> viewMarketplace(
+        @Header("X-R-DID")
+        String did,
+
+        @Header("X-R-DT")
+        String dt,
+
+        @Header("X-R-MAIL")
+        String mail,
+
+        @Header("X-R-AUTH")
+        String auth,
+
+        @Body
+        JsonMarketplace jsonMarketplace
+    );
+
+    /**
+     * Errors
+     * {@link com.noqapp.android.common.beans.JsonResponse#response} is false(0) when not found
+     */
+    @POST("api/c/initiateContact")
+    Call<JsonResponse> initiateContact(
+        @Header("X-R-DID")
+        String did,
+
+        @Header("X-R-DT")
+        String dt,
+
+        @Header("X-R-MAIL")
+        String mail,
+
+        @Header("X-R-AUTH")
+        String auth,
+
+        @Body
+        JsonMarketplace jsonMarketplace
     );
 }
