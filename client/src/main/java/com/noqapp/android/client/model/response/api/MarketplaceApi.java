4ea8d54bac9916c3f6bc821ff7a91b239461dd5a
hth <hth@users.noreply.github.com>
2021-08-15 04:59:45 +0530

    Added api to post on marketplace

--- client/src/main/java/com/noqapp/android/client/model/response/api/MarketplaceApi.java
+++ client/src/main/java/com/noqapp/android/client/model/response/api/MarketplaceApi.java
@@ -32,6 +32,32 @@ public interface MarketplaceApi {
     );
 
     /**
+     * Create new post on marketplace
+     *
+     * Errors
+     * {@link com.noqapp.android.common.beans.JsonResponse#response} is false(0) when not found
+     */
+    @POST("api/c/marketplace")
+    Call<JsonResponse> postOnMarketplace(
+        @Header("X-R-DID")
+        String did,
+
+        @Header("X-R-DT")
+        String dt,
+
+        @Header("X-R-MAIL")
+        String mail,
+
+        @Header("X-R-AUTH")
+        String auth,
+
+        @Body
+        JsonMarketplace jsonMarketplace
+    );
+
+    /**
+     * On clicking view detail on the post.
+     *
      * Errors
      * {@link com.noqapp.android.common.beans.JsonResponse#response} is false(0) when not found
      */
@@ -54,6 +80,8 @@ public interface MarketplaceApi {
     );
 
     /**
+     * Person should be able to do it once. After that the button should be disabled.
+     *
      * Errors
      * {@link com.noqapp.android.common.beans.JsonResponse#response} is false(0) when not found
      */
