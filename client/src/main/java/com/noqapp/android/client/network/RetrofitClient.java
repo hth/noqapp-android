8f3aa131a9498cf3f7c8d69c679437482a00ec6e
hth <hth@users.noreply.github.com>
2021-11-21 07:57:02 +0530

    Added logging

--- client/src/main/java/com/noqapp/android/client/network/RetrofitClient.java
+++ client/src/main/java/com/noqapp/android/client/network/RetrofitClient.java
@@ -9,22 +9,24 @@ import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 /**
- * User: omkar
+ * User: hitender
  * Date: 3/26/17 11:52 PM
  */
 public class RetrofitClient {
     private static Retrofit retrofit = null;
+    private static HttpLoggingInterceptor logging = null;
     private static long TIME_OUT = 35;
 
     public static Retrofit getClient() {
         if (null == retrofit) {
             OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                    .readTimeout(TIME_OUT, TimeUnit.SECONDS)
-                    .connectTimeout(TIME_OUT, TimeUnit.SECONDS);
+                .readTimeout(TIME_OUT, TimeUnit.SECONDS)
+                .connectTimeout(TIME_OUT, TimeUnit.SECONDS);
 
             builder.addInterceptor(chain -> {
                 Request request = chain.request().newBuilder()
@@ -37,13 +39,20 @@ public class RetrofitClient {
                         .addHeader("x-r-mail", AppInitialize.getMail())
                         .addHeader("x-r-qid", AppInitialize.getUserProfile() == null ? "" : AppInitialize.getUserProfile().getQueueUserId()).build();
                 return chain.proceed(request);
-            });
+            }).addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.HEADERS));
+
+            if (BuildConfig.DEBUG) {
+                logging = new HttpLoggingInterceptor();
+                logging.setLevel(HttpLoggingInterceptor.Level.HEADERS);
+
+                builder.addInterceptor(logging);
+            }
 
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BuildConfig.NOQAPP_MOBILE)
-                    .addConverterFactory(JacksonConverterFactory.create())
-                    .client(builder.build())
-                    .build();
+                .baseUrl(BuildConfig.NOQAPP_MOBILE)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(builder.build())
+                .build();
         }
         return retrofit;
     }
