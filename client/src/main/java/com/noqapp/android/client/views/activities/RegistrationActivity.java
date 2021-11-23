70762c635a5cf1150796b0a8fe3c9caa38f81068
hth <hth@users.noreply.github.com>
2021-11-23 16:25:47 +0530

    Get value from invite code

--- client/src/main/java/com/noqapp/android/client/views/activities/RegistrationActivity.java
+++ client/src/main/java/com/noqapp/android/client/views/activities/RegistrationActivity.java
@@ -42,6 +42,7 @@ public class RegistrationActivity extends BaseActivity implements ProfilePresent
     private TextView tv_birthday;
     private EditText edt_pwd;
     private EditText edt_confirm_pwd;
+    private EditText edt_inviteCode;
     private TextView tv_male;
     private TextView tv_female;
     private TextView tv_transgender;
@@ -66,6 +67,7 @@ public class RegistrationActivity extends BaseActivity implements ProfilePresent
         tv_birthday = findViewById(R.id.tv_birthday);
         edt_pwd = findViewById(R.id.edt_pwd);
         edt_confirm_pwd = findViewById(R.id.edt_confirm_pwd);
+        edt_inviteCode = findViewById(R.id.edt_inviteCode);
         tv_male = findViewById(R.id.tv_male);
         tv_female = findViewById(R.id.tv_female);
         tv_transgender = findViewById(R.id.tv_transgender);
@@ -259,9 +261,7 @@ public class RegistrationActivity extends BaseActivity implements ProfilePresent
         registration.setGender(gender);
         registration.setTimeZoneId(tz.getID());
         registration.setCountryShortName(getIntent().getStringExtra("countryShortName"));
-        // Invite code is dropped for the registration form here in favor of identification
-        // registration while acquiring token.
-        registration.setInviteCode("");
+        registration.setInviteCode(edt_inviteCode.getText().toString());
         new AccountClientImpl(this).register(UserUtils.getDeviceId(), registration);
     }
 
