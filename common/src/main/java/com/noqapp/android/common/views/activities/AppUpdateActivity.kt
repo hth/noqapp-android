65e994bfdc6a93753fcd1d46fb651666c1349ac0
hth <hth@users.noreply.github.com>
2021-12-11 10:01:15 +0530

    Check for deprecated method on FLAG_FULLSCREEN

--- common/src/main/java/com/noqapp/android/common/views/activities/AppUpdateActivity.kt
+++ common/src/main/java/com/noqapp/android/common/views/activities/AppUpdateActivity.kt
@@ -1,7 +1,9 @@
 package com.noqapp.android.common.views.activities
 
+import android.os.Build
 import android.os.Bundle
 import android.view.Window
+import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -12,7 +14,12 @@ class AppUpdateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
         setContentView(R.layout.act_app_update)
         val btn_download = findViewById<Button>(R.id.btn_download)
         btn_download.setOnClickListener {
